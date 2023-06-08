6c21a5db805f153d34b8731ec4354b8fe2d98e5e
pprevautel <philippe.prevautel@ign.fr>
2023-06-08 10:05:51 +0200

    fiche de données en cours (ref #5)

---
+++ assets/components/Utils/MapWrapper.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useRef, useEffect } from "react";
+import PropTypes from "prop-types";
+
+import Map from "ol/Map";
+import View from "ol/View";
+import { fromLonLat } from "ol/proj";
+import WMTSCapabilities from "ol/format/WMTSCapabilities";
+import { optionsFromCapabilities } from "ol/source/WMTS";
+import TileLayer from "ol/layer/Tile";
+import WMTS from "ol/source/WMTS";
+
+
+const MapWrapper = (props) => {
+    const [ map, setMap ] = useState();
+
+    // get ref to div element - OpenLayers will render into this div
+    const mapElement = useRef();
+
+    useEffect(() => {
+        const center = props.center ?? [2.35, 48.85];  // Paris
+
+        // create map
+        const initialMap = new Map({
+            target: mapElement.current,
+            view: new View({
+                projection: "EPSG:3857",
+                center: fromLonLat(center),
+                zoom: props.zoom
+            }),
+            controls: []
+        });
+        setMap(initialMap);
+			
+        // Ajout de la couche PLANIGNV2
+        fetch("https://wxs.ign.fr/cartes/geoportail/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities")
+            .then((response) => {
+                if (! response.ok) {
+                    throw response.statusText;
+                }
+                return response.text();
+            }).then((response) => {
+                const format = new WMTSCapabilities();
+                const capabilities = format.read(response);
+
+                const wmtsOptions = optionsFromCapabilities(capabilities, {
+                    layer: "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
+                    matrixSet: "EPSG:3857"
+                });
+
+                const layer = new TileLayer({
+                    opacity: 1,
+                    source: new WMTS(wmtsOptions)
+                });
+                initialMap.addLayer(layer);
+            }).catch(error => { console.log(error); });
+			
+        /* We set map target to undefined to represent a
+         * nonexistent HTML element ID, when the React component is unmounted.
+         * This prevents multiple maps being added to the map container on a
+         * re-render.
+         */
+        return () => initialMap.setTarget(undefined);
+    },[]);
+
+    return (
+        <div ref={mapElement} className={props.className} id={props.id}></div>
+    );
+};
+
+MapWrapper.propTypes = {
+    id: PropTypes.string.isRequired,
+    className: PropTypes.string,
+    zoom: PropTypes.number.isRequired,
+    center: PropTypes.arrayOf(PropTypes.number)
+};
+
+export default MapWrapper;
--- assets/pages/data/DataNewForm.jsx
+++ assets/pages/data/DataNewForm.jsx
@@ -1,21 +1,69 @@
+import { fr } from "@codegouvfr/react-dsfr";
 import { Input } from "@codegouvfr/react-dsfr/Input";
+import { Select } from "@codegouvfr/react-dsfr/Select"; 
 import { yupResolver } from "@hookform/resolvers/yup";
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
-
+import { v4 as uuidv4 } from "uuid";
 import Button from "@codegouvfr/react-dsfr/Button";
 import AppLayout from "../../components/Layout/AppLayout";
+import { defaultProjections } from "../../config/projections";
+// import Autocomplete from "../../components/Utils/Autocomplete";
+import MapWrapper from "../../components/Utils/MapWrapper";
+import "./../../sass/components/zoom-range-map.scss";
+
+const maxFileSize =  2000000000; // 2 GB
+const fileExtensions = ["csv", "gpkg", "zip"]; // TODO zip
+
+const getExtension = filename => {
+    if (! filename) return "";
+    return  filename.split(".").pop().toLowerCase();
+};
 
 const schema = yup
     .object({
         data_name: yup.string().required("Le nom de la donnée est obligatoire"),
-        nombre_test: yup.string().min(5, "ça craint 😱").max(10, "oh mon dieu 😎").required("c'est requis"),
+        date_file: yup.mixed()
+            .required("Un fichier associé est obligatoire")
+            .test("is-valid-size", `La taille maximale pour un fichier est de ${maxFileSize}`,
+                value => value && value.size <= maxFileSize)
+            .test("is-valid-extension", "Seuls les types de fichiers zip, gpkg et csv sont autorisés", 
+                value => value && fileExtensions.includes(getExtension(value.name))
+            )   
     })
     .required();
 
 const DataNewForm = ({ datastoreId }) => {
+    let uuid = "";
+
+    const maxChunkSize = 16000000; // 16 MB
+    const uploadChunk       = Routing.generate("cartesgouvfr_app_upload_chunk");
+    const uploadComplete    = Routing.generate("cartesgouvfr_app_upload_complete");
+
+    const [showDataInfos, setshowDataInfos] = useState(false);
+    const [srid, setSrid] = useState(null);
+    const [projections, setProjections] = useState(defaultProjections);
+
+    useEffect(() => {
+        if (! srid) return;
+        if (srid in projections) {
+            return;
+        }
+
+        const match = srid.match(/EPSG:(\d+)/);
+        if (match) {
+            fetch(`https://epsg.io/${match[1]}.json`)
+                .then(response => response.json())
+                .then(data => {
+                    setProjections([...projections, { code: srid, name: data.name }]); 
+                    setSrid(srid); 
+                })
+                .catch(err => console.log(err));
+        }
+    }, [showDataInfos]);
+
     const {
         register,
         handleSubmit,
@@ -26,6 +74,71 @@ const DataNewForm = ({ datastoreId }) => {
         console.log(errors);
         console.log(formData);
     };
+
+    const createFileChunk = file => {
+        const fileChunkList = [];
+        
+        let cur = 0;
+        while (cur < file.size) {
+            const chunk = file.slice(cur, cur + maxChunkSize);
+            fileChunkList.push({ chunk: chunk, size: chunk.size });
+            cur += maxChunkSize;
+        }
+        return fileChunkList;
+    };
+
+    const handleFileChanged = e => {
+        const file = e.currentTarget.files[0];
+        
+        const extension = getExtension(file.name);
+        if (! fileExtensions.includes(extension)) {
+            e.currentTarget.value = "";
+            // TODO Envoyer une erreur propre
+            return;
+        }
+
+        uuid = uuidv4();
+        const chunks = createFileChunk(file);
+
+        const promises = chunks.map((chunkProps, index) => {
+            const chunkFile = new File([chunkProps.chunk], "chunk");
+            const formData = new FormData();
+            
+            formData.append("uuid", uuid);
+            formData.append("chunk", chunkFile, `${uuid}_${index}`);
+            formData.append("index", index);
+            return fetch(uploadChunk, { 
+                method: "POST",
+                body: formData 
+            }).then(() => {
+
+            });
+        });
+        
+        Promise.all(promises).then(() => {
+            // Tout s'est bien passe, on merge tous les fichiers
+            const formData = new FormData();
+            formData.append("uuid", uuid);
+            formData.append("originalFilename", file.name);
+
+            fetch(uploadComplete, { method: "POST", body: formData })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.srid) {
+                        setSrid(data.srid);
+                    }
+                    setshowDataInfos(true);
+                }).catch(err => {
+                    console.log(err.message); 
+                });
+
+        }).catch(err => { 
+            uuid = "";
+            console.log(err.message); 
+        });
+    };
+
+    const fileProps = { type: "file", onChange: handleFileChanged };
 
     return (
         <AppLayout>
@@ -39,11 +152,49 @@ const DataNewForm = ({ datastoreId }) => {
                 nativeInputProps={register("data_name")}
             />
             <Input
-                label="Nombre test"
-                state={errors.nombre_test ? "error" : "default"}
-                stateRelatedMessage={errors?.nombre_test?.message}
-                nativeInputProps={register("nombre_test")}
+                label="Déposez votre fichier de données"
+                hintText="Formats de fichiers autorisés : archive zip contenant un Geopackage (recommandé) ou un CSV..."
+                state={errors.data_file ? "error" : "default"}
+                stateRelatedMessage={errors?.data_name?.message}
+                // nativeInputProps={{ type: "file", onChange: handleFileChanged}}
+                nativeInputProps={{...register("data_file"), ...fileProps}}
             />
+            <MapWrapper id={"map-xxxxxx"} zoom={5} className={"zoom-range-map"} />
+            {
+                showDataInfos && 
+                <div className={fr.cx("fr-mt-2v")}>
+                    <h5>Les données suivantes ont été détectées. Modifiez les si besoins</h5>
+                    <Input
+                        label="Nom technique de votre donnée:"
+                        hintText="Ce nom technique est invisible par votre utilisateur final. Il apparaitra uniquement dans votre espace de travail"
+                        state={errors.data_technical_name ? "error" : "default"}
+                        stateRelatedMessage={errors?.data_technical_name?.message}
+                    />
+                    {/* <Autocomplete minChar={3} /> */}
+                    <Select
+                        label="Projection de vos données"
+                        nativeSelectProps={{
+                            onChange: event => setSrid(event.target.value)
+                        }}
+                    >
+                        {srid === null ? (
+                            <option value="" disabled selected hidden>Selectionnez une Projection</option>
+                        ) : ( 
+                            <option value="" disabled hidden>Selectionnez une Projection</option>
+                        )}
+                        {
+                            projections.map((proj) => { 
+                                if (proj.code === srid) {
+                                    return <option key={proj.code} value={proj.code} selected>{proj.name}</option>;
+                                } else {
+                                    return <option key={proj.code} value={proj.code}>{proj.name}</option>;
+                                }
+                            })                           
+                        }
+                    </Select>
+                </div>
+                
+            }
             <Button onClick={handleSubmit(onSubmit)}>Soumettre</Button>
         </AppLayout>
     );
