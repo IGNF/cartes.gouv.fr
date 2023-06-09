722efaa3080e0305b826384a7e16fcd54cb4cc14
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-06-09 13:58:25 +0200

    chore: changements mineurs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/*eslint no-undef: 0*/
 module.exports = {
     env: {
         browser: true,
--- assets/components/Utils/BtnBackToDashboard.jsx
+++ assets/components/Utils/BtnBackToDashboard.jsx
@@ -5,9 +5,9 @@ import React from "react";
 
 import { routes } from "../../router/router";
 
-const BtnBackToDashboard = ({ datastoreId }) => {
+const BtnBackToDashboard = ({ datastoreId, className }) => {
     return (
-        <Button linkProps={routes.datastore_dashboard({ datastoreId }).link} className={fr.cx("fr-my-2w")}>
+        <Button linkProps={routes.datastore_dashboard({ datastoreId }).link} className={fr.cx("fr-my-2w", className)}>
             {"Retour au tableau de bord"}
         </Button>
     );
@@ -15,6 +15,7 @@ const BtnBackToDashboard = ({ datastoreId }) => {
 
 BtnBackToDashboard.propTypes = {
     datastoreId: PropTypes.string.isRequired,
+    className: PropTypes.string,
 };
 
 export default BtnBackToDashboard;
