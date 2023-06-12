24b082d8da8d4641bc39b5ee79992df5d444790f
pprevautel <philippe.prevautel@ign.fr>
2023-06-12 08:03:38 +0200

    refactor(Progress): (ref #5)

--- assets/components/Utils/Progress.jsx
+++ assets/components/Utils/Progress.jsx
@@ -3,32 +3,31 @@ import PropTypes from "prop-types";
 import React, { useState, useEffect } from "react";
 import "./../../sass/components/progress.scss";
  
-const Progress = ({label, value}) => {
+const Progress = ({label, value, max = 100}) => {
     const [percent, setPercent] = useState("");
 
     useEffect(() => {
-        let round = Math.floor(value);
-        setPercent(`${round}%`);
+        let percent = Math.floor(value / max) * 100;
+        setPercent(`${percent}%`);
     }, [value]);
     return (
-        <>
-            <div className={fr.cx("fr-my-2v")}>
-                <div className={fr.cx("fr-input-group")}>
-                    { label && (
-                        <label className={fr.cx("fr-label")}>{label}</label>
-                    )}
-                    <div className="progress">
-                        <div className="progress-bar" style={{width: percent}}>{percent}</div>    
-                    </div>
+        <div className={fr.cx("fr-my-2v")}>
+            <div className={fr.cx("fr-input-group")}>
+                { label && (
+                    <label className={fr.cx("fr-label")}>{label}</label>
+                )}
+                <div className="progress">
+                    <div className="progress-bar" style={{width: percent}}>{percent}</div>    
                 </div>
             </div>
-        </>
+        </div>
     );
 };
 
 Progress.propTypes = {
     label: PropTypes.string,
-    value: PropTypes.number
+    value: PropTypes.number,
+    max: PropTypes.number
 };
 
 export default Progress;
