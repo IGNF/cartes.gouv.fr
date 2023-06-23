01527bb1f5563532dedf70813f172dfeaf0b5e0a
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-06-23 13:00:24 +0200

    change: publicRoutes -> protectedRoutes, app_root -> appRoot * gèrer une liste de routes publiques plutôt que protectedRoutes, parce qu'on oublie parfois d'ajouter une nouvelle route dans la liste protectedRoutes, donc par défaut toutes les routes sont protégées * changement de nom de variable app_root à appRoot, et appRoot est exporté depuis le fichier router.js

--- assets/router/router.js
+++ assets/router/router.js
@@ -1,43 +1,43 @@
 import { createRouter, defineRoute, param } from "type-route";
 
-const app_root = document.getElementById("root").dataset?.appRoot;
+export const appRoot = document.getElementById("root").dataset?.appRoot;
 
 const routeDefs = {
     // routes non protégées
-    home: defineRoute(app_root === "" ? "/" : app_root),
-    docs: defineRoute(`${app_root}/documentation`),
-    contact: defineRoute(`${app_root}/nous-ecrire`),
-    news: defineRoute(`${app_root}/actualites`),
-    sitemap: defineRoute(`${app_root}/plan-du-site`),
-    accessibility: defineRoute(`${app_root}/accessibilite`),
-    legal_notice: defineRoute(`${app_root}/mentions-legales`),
-    cgu: defineRoute(`${app_root}/conditions-generales-d-utilisation`),
-    personal_data: defineRoute(`${app_root}/donnees-personnelles`),
-    cookies: defineRoute(`${app_root}/gestion-des-cookies`),
+    home: defineRoute(appRoot === "" ? "/" : appRoot),
+    docs: defineRoute(`${appRoot}/documentation`),
+    contact: defineRoute(`${appRoot}/nous-ecrire`),
+    news: defineRoute(`${appRoot}/actualites`),
+    sitemap: defineRoute(`${appRoot}/plan-du-site`),
+    accessibility: defineRoute(`${appRoot}/accessibilite`),
+    legal_notice: defineRoute(`${appRoot}/mentions-legales`),
+    cgu: defineRoute(`${appRoot}/conditions-generales-d-utilisation`),
+    personal_data: defineRoute(`${appRoot}/donnees-personnelles`),
+    cookies: defineRoute(`${appRoot}/gestion-des-cookies`),
 
     // routes protégées
     // utilisateur
-    my_account: defineRoute(`${app_root}/mon-compte`),
+    my_account: defineRoute(`${appRoot}/mon-compte`),
 
     // espaces de travail
-    datastore_list: defineRoute(`${app_root}/datastores`),
+    datastore_list: defineRoute(`${appRoot}/datastores`),
     datastore_dashboard: defineRoute(
         {
             datastoreId: param.path.string,
         },
-        (p) => `${app_root}/datastores/${p.datastoreId}`
+        (p) => `${appRoot}/datastores/${p.datastoreId}`
     ),
     datastore_data_list: defineRoute(
         {
             datastoreId: param.path.string,
         },
-        (p) => `${app_root}/datastores/${p.datastoreId}/data`
+        (p) => `${appRoot}/datastores/${p.datastoreId}/data`
     ),
     datastore_data_new: defineRoute(
         {
             datastoreId: param.path.string,
         },
-        (p) => `${app_root}/datastores/${p.datastoreId}/data/new`
+        (p) => `${appRoot}/datastores/${p.datastoreId}/data/new`
     ),
 
     // Creer et publier un service WFS
@@ -46,10 +46,21 @@ const routeDefs = {
             datastoreId: param.path.string,
             storedDataId: param.path.string,
         },
-        (p) => `${app_root}/datastores/${p.datastoreId}/data/${p.storedDataId}/service/wfs/new`
+        (p) => `${appRoot}/datastores/${p.datastoreId}/data/${p.storedDataId}/service/wfs/new`
     ),
 };
 
 export const { RouteProvider, useRoute, routes } = createRouter(routeDefs);
 
-export const protectedRoutes = ["my_account", "datastore_list", "datastore_dashboard", "datastore_data_new", "datastore_wfs_service_new"];
+export const publicRoutes = [
+    "home",
+    "docs",
+    "contact",
+    "news",
+    "sitemap",
+    "accessibility",
+    "legal_notice",
+    "cgu",
+    "personal_data",
+    "cookies",
+];
