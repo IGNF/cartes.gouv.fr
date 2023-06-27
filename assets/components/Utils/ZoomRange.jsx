038b4aaf0af6efc6a89e1a0c68ba1c7d7cfe63c0
pprevautel <philippe.prevautel@ign.fr>
2023-06-27 08:58:05 +0200

    refactor: ZoomRange (utilisation du slider de MUI)

--- assets/components/Utils/ZoomRange.jsx
+++ assets/components/Utils/ZoomRange.jsx
@@ -1,7 +1,8 @@
 import { fr } from "@codegouvfr/react-dsfr";
+import React, { useId, useState, useEffect } from "react";
 import PropTypes from "prop-types";
-import React, { useState, useEffect, useRef } from "react";
-import RangeSlider from "./RangeSlider";
+
+// Openlayers
 import Map from "ol/Map";
 import View from "ol/View";
 import { fromLonLat } from "ol/proj";
@@ -9,66 +10,48 @@ import WMTSCapabilities from "ol/format/WMTSCapabilities";
 import { optionsFromCapabilities } from "ol/source/WMTS";
 import TileLayer from "ol/layer/Tile";
 import WMTS from "ol/source/WMTS";
-
 import "./../../sass/components/zoom-range.scss";
+import RangeSlider from "./RangeSlider";
 
+const ZoomRange = (props) => {
+    const {
+        min = 0, 
+        max = 20, 
+        initialValues = [0, 20],
+        center = [2.35, 48.85],
+        onChange = null
+    } = props;
 
-const ZoomRange = ({
-    min, 
-    max, 
-    initialMinValue, 
-    initialMaxValue, 
-    center,
-    onChange = null,
-    minFixed = false,
-    maxFixed = false}) => {
-    const mapCenter = center ?? [2.35, 48.85];  // Paris
- 
-    const minZoomMapElement = useRef();
+    /* Identifiants des div contenant les maps */
+    const numMaps = 2;
+    const targets = [...new Array(numMaps)].map(() => "map-" + useId());
+   
     const [minZoomMap, setMinZoomMap] = useState(null);
-    
-    const maxZoomMapElement = useRef();
     const [maxZoomMap, setMaxZoomMap] = useState(null);
 
-    const handleOnChange = value => {
-        if (value.minValue != minZoomMap.getView().getZoom()) {
-            minZoomMap.getView().setZoom(value.minValue);    
-        }
-        if (value.maxValue != maxZoomMap.getView().getZoom()) {
-            maxZoomMap.getView().setZoom(value.maxValue);    
-        }
-        if (onChange) {
-            onChange(value);
-        }
-    };
+    const [values, setValues] = useState([
+        Math.max(min, initialValues[0]), 
+        Math.min(max, initialValues[1])
+    ]);
 
     useEffect(() => {
-        // create map
-        const map1 = new Map({
-            target: minZoomMapElement.current,
-            view: new View({
-                projection: "EPSG:3857",
-                center: fromLonLat(mapCenter),
-                zoom: initialMinValue
-            }),
-            controls: [],
-            interactions: []
-        });
-        setMinZoomMap(map1);
-			
-        const map2 = new Map({
-            target: maxZoomMapElement.current,
-            view: new View({
-                projection: "EPSG:3857",
-                center: fromLonLat(mapCenter),
-                zoom: initialMaxValue
-            }),
-            controls: [],
-            interactions: []
-        });
-        setMaxZoomMap(map2);
+        const maps = [];
+        for (let m=0; m<numMaps; ++m) {
+            maps.push(new Map({
+                target: targets[m],
+                view: new View({
+                    projection: "EPSG:3857",
+                    center: fromLonLat(center),
+                    zoom: values[m]
+                }),
+                controls: [],
+                interactions: []
+            }));   
+        }
+        setMinZoomMap(maps[0]);
+        setMaxZoomMap(maps[1]);
 
-        // Ajout de la couche PLANIGNV2
+        /* Recuperation du GetCapabilities pour avoir les informations du flux PLANIGNV2 */
         fetch("https://wxs.ign.fr/cartes/geoportail/wmts?SERVICE=WMTS&VERSION=1.0.0&REQUEST=GetCapabilities")
             .then((response) => {
                 if (! response.ok) {
@@ -83,41 +66,44 @@ const ZoomRange = ({
                     layer: "GEOGRAPHICALGRIDSYSTEMS.PLANIGNV2",
                     matrixSet: "EPSG:3857"
                 });
-
-                map1.addLayer(new TileLayer({
-                    opacity: 1,
-                    source: new WMTS(wmtsOptions)
-                }));
-                map2.addLayer(new TileLayer({
-                    opacity: 1,
-                    source: new WMTS(wmtsOptions)
-                }));
+                
+                for (let m=0; m<numMaps; ++m) {
+                    maps[m].addLayer(new TileLayer({
+                        opacity: 1,
+                        source: new WMTS(wmtsOptions)
+                    }));
+                }
             }).catch(error => { console.log(error); });
-			
+
         /* We set map target to undefined to represent a
          * nonexistent HTML element ID, when the React component is unmounted.
          * This prevents multiple maps being added to the map container on a
          * re-render.
          */
         return () => {
-            map1.setTarget(undefined);
-            map2.setTarget(undefined);
+            for (let m=0; m<numMaps; ++m) {
+                maps[m].setTarget(undefined);
+            }
         };
     },[]);
 
-    return (
+    const handleOnChange = values => {
+        setValues(values);
+        minZoomMap?.getView().setZoom(values[0]);  
+        maxZoomMap?.getView().setZoom(values[1]);
+        onChange?.onChange(values);
+    };
+
+    return (  
         <div className={fr.cx("fr-my-2v")}>
             <div className="ui-map-zoom-levels">
-                <div ref={minZoomMapElement} className="ui-top-zoom-level"></div>
-                <div ref={maxZoomMapElement} className="ui-bottom-zoom-level"></div>
+                <div id={targets[0]} className="ui-top-zoom-level"></div>
+                <div id={targets[1]} className="ui-bottom-zoom-level"></div>
             </div>
-            <RangeSlider 
-                min={min} 
-                max={max} 
-                initialMinValue={initialMinValue} 
-                initialMaxValue={initialMaxValue} 
-                minFixed={minFixed} 
-                maxFixed={maxFixed} 
+            <RangeSlider
+                min={min}
+                max={max}
+                initialValues={initialValues}
                 onChange={handleOnChange}
             />
         </div>
@@ -125,14 +111,11 @@ const ZoomRange = ({
 };
 
 ZoomRange.propTypes = {
-    min: PropTypes.number.isRequired,               // La valeur minimale possible pour le zoom
-    max: PropTypes.number.isRequired,               // La valeur maximale possible pour le zoom
-    initialMinValue: PropTypes.number,              // La valeur min initiale du zoom
-    initialMaxValue: PropTypes.number,              // La valeur max initiale du zoom
-    center: PropTypes.arrayOf(PropTypes.number),    // Le centre de la carte en lon/lat
-    onChange: PropTypes.func,                       // function callback du parent
-    minFixed: PropTypes.bool,                       // Est-ce que la valeur du zoom min est fixe
-    maxFixed: PropTypes.bool                        // Est-ce que la valeur du zoom max est fixe
+    min: PropTypes.number, 
+    max : PropTypes.number,
+    initialValues: PropTypes.arrayOf(PropTypes.number), 
+    center: PropTypes.arrayOf(PropTypes.number),
+    onChange: PropTypes.func
 };
-
+ 
 export default ZoomRange;
--- assets/pages/service/wfs/forms/AccessResctrictionForm.jsx
+++ assets/pages/service/wfs/forms/AccessResctrictionForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 
-const  = () => {
+const AccessRestrictionForm = () => {
     return (  );
 }
  
