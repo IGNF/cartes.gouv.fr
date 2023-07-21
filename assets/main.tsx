cbda501e8f26bac8b437be42d4bceec42dba981e
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-07-21 12:49:30 +0200

    feat: désactiver les extensions react-dev-tools des navigateurs en prod

--- assets/main.tsx
+++ assets/main.tsx
@@ -1,8 +1,13 @@
 import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
+
+if ((document.getElementById("root") as HTMLDivElement)?.dataset?.appEnv === "prod") {
+    disableReactDevTools();
+}
 
 startReactDsfr({ defaultColorScheme: "light" });
 
