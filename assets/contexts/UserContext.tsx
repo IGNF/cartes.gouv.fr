0cb976f4db0777cf859006d49948e6d51d3deb63
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-07-27 15:39:25 +0200

    feat(UserContext): mise à jour types

--- assets/contexts/UserContext.tsx
+++ assets/contexts/UserContext.tsx
@@ -1,14 +1,32 @@
 import { Dispatch, FC, PropsWithChildren, createContext, useState } from "react";
 
-type UserType = object;
+/** déclaration du type `User`, à tenir à jour avec le User de symfony */
+export type UserType = {
+    id: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    roles: string[];
+    communitiesMember: string[];
+    accountCreationDate: string;
+    lastApiCallDate: string;
+};
 
+/** déclaration du type de la valeur du contexte `UserContext` */
 type UserContextType = {
-    user: UserType | null | undefined;
+    user?: UserType;
+
+    /** le setter d'un useState */
     setUser: Dispatch<UserType>;
 };
 
-export const UserContext = createContext<null | UserContextType>(null);
+/** initialisation du contexte `UserContext` */
+export const UserContext = createContext<UserContextType>({
+    user: undefined,
+    setUser: () => {},
+});
 
+/** le Provider du contexte `UserContext` */
 export const UserContextProvider: FC<PropsWithChildren> = ({ children }) => {
     const userFromTwig = (document.getElementById("user") as HTMLDivElement).dataset?.user ?? null;
     const [user, setUser] = useState<UserType>(userFromTwig === null ? null : JSON.parse(userFromTwig));
