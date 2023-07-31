fd07639a220509d616d5316f8ba90b6e1e2919ff
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-07-31 17:59:31 +0200

    feat: ajoute méthodes dans Datastore & Configuration ApiService

--- assets/api/datastore.ts
+++ assets/api/datastore.ts
@@ -1,7 +1,7 @@
 import Routing from "fos-router";
 
 import { jsonFetch } from "../modules/jsonFetch";
-import { Datastore } from "../types/app";
+import { type Datastore, type DatastoreEndpoint } from "../types/app";
 
 const get = (datastoreId: string, otherOptions: RequestInit = {}) => {
     const url = Routing.generate("cartesgouvfr_api_datastore_get", { datastoreId });
@@ -10,8 +10,16 @@ const get = (datastoreId: string, otherOptions: RequestInit = {}) => {
     });
 };
 
+const getEndpoints = (datastoreId: string, query: { type?: string; open?: boolean }, otherOptions: RequestInit = {}) => {
+    const url = Routing.generate("cartesgouvfr_api_datastore_get_endpoints", { datastoreId, ...query });
+    return jsonFetch<DatastoreEndpoint[]>(url, {
+        ...otherOptions,
+    });
+};
+
 const datastore = {
     get,
+    getEndpoints,
 };
 
 export default datastore;
--- src/Services/EntrepotApi/DatastoreApiService.php
+++ src/Services/EntrepotApi/DatastoreApiService.php
@@ -8,4 +8,37 @@ class DatastoreApiService extends AbstractEntrepotApiService
     {
         return $this->request('GET', "datastores/$datastoreId");
     }
+
+    /**
+     * @param array<string,mixed> $query
+     */
+    public function getEndpoints(string $datastoreId, array $query = []): array
+    {
+        $datastore = $this->get($datastoreId);
+        $endpoints = $datastore['endpoints'];
+
+        if (isset($query['type'])) {
+            $endpoints = array_filter($endpoints, function ($endpoint) use ($query) {
+                if (isset($endpoint['endpoint']['type'])) {
+                    return strtoupper($endpoint['endpoint']['type']) === strtoupper($query['type']);
+                }
+
+                return false;
+            });
+        }
+
+        if (isset($query['open'])) {
+            $endpoints = array_filter($endpoints, function ($endpoint) use ($query) {
+                if (isset($endpoint['endpoint']['open'])) {
+                    return $endpoint['endpoint']['open'] === $query['open'];
+                }
+
+                return false;
+            });
+        }
+
+        $endpoints = array_values($endpoints);
+
+        return $endpoints;
+    }
 }
