120786f5986681668f1247ab867e3f94a7609646
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-07-31 17:56:55 +0200

    feat: ajoute vérification sur la methode de GET de StoredDataController

--- src/Controller/Api/StoredDataController.php
+++ src/Controller/Api/StoredDataController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller\Api;
 
+use App\Exception\CartesApiException;
+use App\Exception\EntrepotApiException;
 use App\Services\EntrepotApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route(
@@ -23,6 +26,17 @@ class StoredDataController extends AbstractController
     #[Route('/{storedDataId}', name: 'get', methods: ['GET'])]
     public function getStoredData(string $datastoreId, string $storedDataId): JsonResponse
     {
-        return $this->json($this->entrepotApiService->storedData->get($datastoreId, $storedDataId));
+        try {
+            $storedData = $this->entrepotApiService->storedData->get($datastoreId, $storedDataId);
+
+            return $this->json($storedData);
+        } catch (EntrepotApiException $ex) {
+            if (Response::HTTP_NOT_FOUND === $ex->getStatusCode()) {
+                throw new CartesApiException("La donnée stockée [$storedDataId] n'existe pas", $ex->getStatusCode(), $ex->getDetails());
+            } elseif (Response::HTTP_BAD_REQUEST === $ex->getStatusCode()) {
+                throw new CartesApiException("L'identifiant de la donnée stockée [$storedDataId] est invalide", $ex->getStatusCode(), $ex->getDetails());
+            }
+            throw new CartesApiException($ex->getMessage(), $ex->getStatusCode(), $ex->getDetails());
+        }
     }
 }
