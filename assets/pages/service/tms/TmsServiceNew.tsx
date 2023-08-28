853b8e2eb7acb9505bda0079b157dadc46f274b5
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-08-28 19:10:12 +0200

    feat(wms-vector): retire l'étape choix d'attributs, erreurs de maquette

---
+++ assets/pages/service/tms/TmsServiceNew.tsx
@@ -0,0 +1,26 @@
+// ce code pourra être utilisé plus tard pour le tms
+/*
+const schema =yup.object({
+    table_attributes: yup.lazy(() => {
+        return yup.mixed().nullable().notRequired();
+
+        // TODO : validation désactivée en attente de plus de précisions
+        // if (!selectedTables || selectedTables.length === 0) {
+        // return yup.mixed().nullable().notRequired();
+        // }
+
+        // const tableAttributes = {};
+        // selectedTables.forEach((table) => {
+        //     tableAttributes[table.name] = yup
+        //         .array()
+        //         .of(yup.string())
+        //         .min(1, "Veuillez choisir au moins un attribut pour chaque table")
+        //         .required("Veuillez choisir au moins un attribut pour chaque table");
+        // });
+        // return yup.object().shape(tableAttributes);
+    }),
+})
+
+<TableAttributeSelection visible={currentStep === STEPS.TABLE_ATTRIBUTES} selectedTables={selectedTables} form={form} />
+
+*/
