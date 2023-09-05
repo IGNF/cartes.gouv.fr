b90bff1d5a0e84321affddc4522fd776d1dfa5ec
pprevautel <philippe.prevautel@ign.fr>
2023-09-05 15:02:00 +0200

    fix: Ajouter un lien d'évitement ref #50

--- assets/components/Layout/AppLayout.tsx
+++ assets/components/Layout/AppLayout.tsx
@@ -1,20 +1,31 @@
 import { fr } from "@codegouvfr/react-dsfr";
 import { FC, PropsWithChildren } from "react";
-
+import { SkipLinks } from "@codegouvfr/react-dsfr/SkipLinks";
 import { MainNavigationProps } from "@codegouvfr/react-dsfr/MainNavigation/MainNavigation";
 import AppFooter from "./AppFooter";
 import AppHeader from "./AppHeader";
 import { ConsentBannerAndConsentManagement } from "../../config/consentManagement";
+import Translator from "../../modules/Translator";
 
 type AppLayoutProps = {
     navItems?: MainNavigationProps.Item[];
 };
+
 const AppLayout: FC<PropsWithChildren<AppLayoutProps>> = ({ children, navItems = [] }) => {
     return (
         <>
+            {/* Lien d'evitement (skip link) */}
+            <SkipLinks
+                links={[
+                    {
+                        anchor: "#main",
+                        label: Translator.trans("site.go_to_content"),
+                    },
+                ]}
+            />
             <ConsentBannerAndConsentManagement />
             <AppHeader navItems={navItems} />
-            <main role="main" className={fr.cx("fr-py-2w")}>
+            <main id="main" role="main" className={fr.cx("fr-py-2w")}>
                 <div className={fr.cx("fr-container")}>{children}</div>
             </main>
             <AppFooter />
