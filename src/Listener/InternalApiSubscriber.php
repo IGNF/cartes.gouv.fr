321f80f1d3cdfd978d1b688d3d22eda78338cd80
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-10-25 15:30:25 +0200

    fix: corrige condition InternalApiSubscriber

--- src/Listener/InternalApiSubscriber.php
+++ src/Listener/InternalApiSubscriber.php
@@ -6,6 +6,7 @@ use App\Controller\Api\ApiControllerInterface;
 use App\Exception\CartesApiException;
 use App\Security\KeycloakToken;
 use League\OAuth2\Client\Token\AccessToken;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
@@ -17,6 +18,11 @@ use Symfony\Component\HttpKernel\KernelEvents;
  */
 class InternalApiSubscriber implements EventSubscriberInterface
 {
+    public function __construct(
+        private ParameterBagInterface $parameters
+    ) {
+    }
+
     public static function getSubscribedEvents(): array
     {
         return [
@@ -37,17 +43,20 @@ class InternalApiSubscriber implements EventSubscriberInterface
             $controller = $controller[0];
         }
 
+        // vérification de token uniquement si la requête concerne un controller implémentant ApiControllerInterface et que APP_ENV != test
         if ($controller instanceof ApiControllerInterface) {
-            /** @var SessionInterface */
-            $session = $event->getRequest()->getSession();
+            if ('test' !== $this->parameters->get('app_env')) {
+                /** @var SessionInterface */
+                $session = $event->getRequest()->getSession();
 
-            /** @var AccessToken */
-            $accessToken = $session->get(KeycloakToken::SESSION_KEY);
+                /** @var AccessToken */
+                $accessToken = $session->get(KeycloakToken::SESSION_KEY);
 
-            // TODO préciser que c'est l'API interne
-            if (null == $accessToken
-                || (null != $accessToken && $accessToken->hasExpired())) {
-                throw new CartesApiException(Response::$statusTexts[Response::HTTP_UNAUTHORIZED], Response::HTTP_UNAUTHORIZED, ['controller' => ApiControllerInterface::class]);
+                // TODO préciser que c'est l'API interne
+                if (null == $accessToken
+                    || (null != $accessToken && $accessToken->hasExpired())) {
+                    throw new CartesApiException(Response::$statusTexts[Response::HTTP_UNAUTHORIZED], Response::HTTP_UNAUTHORIZED, ['controller' => ApiControllerInterface::class]);
+                }
             }
         }
     }
