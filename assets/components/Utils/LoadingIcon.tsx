35b0f65ec77e4e91f0205f36f4d28db6b4935498
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-12-20 18:47:31 +0100

    feat: améliorations sur LoadingText et LoadingIcon

--- assets/components/Utils/LoadingIcon.tsx
+++ assets/components/Utils/LoadingIcon.tsx
@@ -5,9 +5,10 @@ import "../../sass/components/spinner.scss";
 
 type LoadingIconProps = {
     className?: string;
+    largeIcon?: boolean;
 };
-const LoadingIcon: FC<LoadingIconProps> = ({ className = "" }) => {
-    return <i className={fr.cx("fr-icon-refresh-line", "fr-icon--lg") + " frx-icon-spin " + className} />;
+const LoadingIcon: FC<LoadingIconProps> = ({ className = "", largeIcon = false }) => {
+    return <span className={fr.cx("fr-icon-refresh-line", largeIcon && "fr-icon--lg") + " frx-icon-spin " + className} />;
 };
 
 export default memo(LoadingIcon);
--- assets/components/Utils/LoadingText.tsx
+++ assets/components/Utils/LoadingText.tsx
@@ -1,12 +1,20 @@
 import { ReactNode } from "react";
 
+import LoadingIcon from "./LoadingIcon";
+
 type LoadingTextProps = {
     message?: ReactNode;
     as?: `h${1 | 2 | 3 | 4 | 5 | 6}` | "p";
+    withSpinnerIcon?: boolean;
+    className?: string;
 };
 
-const LoadingText = ({ message, as: HtmlTag = "h1" }: LoadingTextProps) => {
-    return <HtmlTag>{message ? message : "Chargement..."}</HtmlTag>;
+const LoadingText = ({ message, className = "", as: HtmlTag = "h1", withSpinnerIcon = false }: LoadingTextProps) => {
+    return (
+        <HtmlTag className={className}>
+            {message ? message : "Chargement..."} {withSpinnerIcon && <LoadingIcon largeIcon={HtmlTag !== "p"} />}
+        </HtmlTag>
+    );
 };
 
 export default LoadingText;
