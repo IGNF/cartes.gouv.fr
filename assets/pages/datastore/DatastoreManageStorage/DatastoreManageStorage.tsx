69ad1a2a1669541d2a7aca96c625b6fbda23ad10
Orka Arnest CRUZE <orka.cruze@ign.fr>
2023-12-22 14:01:50 +0100

    feat(gestion-espace): affichage conditionnel de l'onglet filesystem si stockage existe (refs #223)

--- assets/pages/datastore/DatastoreManageStorage/DatastoreManageStorage.tsx
+++ assets/pages/datastore/DatastoreManageStorage/DatastoreManageStorage.tsx
@@ -1,11 +1,12 @@
 import { fr } from "@codegouvfr/react-dsfr";
-import Tabs from "@codegouvfr/react-dsfr/Tabs";
+import Tabs, { TabsProps } from "@codegouvfr/react-dsfr/Tabs";
 import { useQuery } from "@tanstack/react-query";
 import { declareComponentKeys } from "i18nifty";
-import { FC, JSX } from "react";
+import { FC, JSX, useMemo } from "react";
 
 import api from "../../../api";
 import DatastoreLayout from "../../../components/Layout/DatastoreLayout";
+import LoadingIcon from "../../../components/Utils/LoadingIcon";
 import { useTranslation, type Translations } from "../../../i18n/i18n";
 import RQKeys from "../../../modules/RQKeys";
 import AnnexeUsage from "./storages/AnnexeUsage";
@@ -14,7 +15,6 @@ import FilesystemUsage from "./storages/FilesystemUsage";
 import PostgresqlUsage from "./storages/PostgresqlUsage";
 import S3Usage from "./storages/S3Usage";
 import UploadUsage from "./storages/UploadUsage";
-import LoadingIcon from "../../../components/Utils/LoadingIcon";
 
 type DatastoreManageStorageProps = {
     datastoreId: string;
@@ -27,6 +27,47 @@ const DatastoreManageStorage: FC<DatastoreManageStorageProps> = ({ datastoreId }
         queryFn: ({ signal }) => api.datastore.get(datastoreId, { signal }),
         staleTime: 3600000,
     });
+
+    const tabs: TabsProps["tabs"] = useMemo(() => {
+        if (datastoreQuery.data === undefined) {
+            return [];
+        }
+
+        // NOTE : d'après les utilisations de l'API vues jusque là, seul le stockage FILESYSTEM est optionnel, les autres sont forcément là
+        const hasFilesystemStorage = datastoreQuery.data?.storages.data.find((data) => data.storage.type === "FILESYSTEM") !== undefined;
+
+        const tabs: TabsProps["tabs"] = [
+            {
+                label: t("storage.postgresql.label"),
+                content: <PostgresqlUsage datastore={datastoreQuery.data} />,
+            },
+            {
+                label: t("storage.s3.label"),
+                content: <S3Usage datastore={datastoreQuery.data} />,
+            },
+            {
+                label: t("storage.upload.label"),
+                content: <UploadUsage datastore={datastoreQuery.data} />,
+            },
+            {
+                label: t("storage.annexe.label"),
+                content: <AnnexeUsage datastore={datastoreQuery.data} />,
+            },
+            {
+                label: t("storage.endpoints.label"),
+                content: <EndpointsUsage datastore={datastoreQuery.data} />,
+            },
+        ];
+
+        if (hasFilesystemStorage) {
+            tabs.unshift({
+                label: t("storage.filesystem.label"),
+                content: <FilesystemUsage datastore={datastoreQuery.data} />,
+            });
+        }
+
+        return tabs;
+    }, [datastoreQuery.data, t]);
 
     return (
         <DatastoreLayout datastoreId={datastoreId} documentTitle={t("title", { datastoreName: datastoreQuery?.data?.name })}>
@@ -41,35 +82,7 @@ const DatastoreManageStorage: FC<DatastoreManageStorageProps> = ({ datastoreId }
             {datastoreQuery.data && (
                 <div className={fr.cx("fr-grid-row")}>
                     <div className={fr.cx("fr-col-12")}>
-                        <Tabs
-                            tabs={[
-                                {
-                                    label: t("storage.filesystem.label"),
-                                    content: <FilesystemUsage datastore={datastoreQuery.data} />,
-                                },
-                                {
-                                    label: t("storage.postgresql.label"),
-                                    content: <PostgresqlUsage datastore={datastoreQuery.data} />,
-                                    isDefault: true,
-                                },
-                                {
-                                    label: t("storage.s3.label"),
-                                    content: <S3Usage datastore={datastoreQuery.data} />,
-                                },
-                                {
-                                    label: t("storage.upload.label"),
-                                    content: <UploadUsage datastore={datastoreQuery.data} />,
-                                },
-                                {
-                                    label: t("storage.annexe.label"),
-                                    content: <AnnexeUsage datastore={datastoreQuery.data} />,
-                                },
-                                {
-                                    label: t("storage.endpoints.label"),
-                                    content: <EndpointsUsage datastore={datastoreQuery.data} />,
-                                },
-                            ]}
-                        />
+                        <Tabs tabs={tabs} />
                     </div>
                 </div>
             )}
