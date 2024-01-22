9e03b3d1714c22156a2fcdc0a102872ed696747c
pprevautel <philippe.prevautel@ign.fr>
2024-01-22 11:35:51 +0100

    Suppression import

--- assets/components/Utils/RMap.tsx
+++ assets/components/Utils/RMap.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { FC, useCallback, useEffect, useMemo, useRef } from "react";
 
 import Map from "ol/Map";
 import View from "ol/View";
