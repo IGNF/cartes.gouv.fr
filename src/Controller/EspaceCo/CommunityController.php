e3c5cd19b58fc9c96b949337c770082be1bae589
pprevautel <philippe.prevautel@ign.fr>
2024-04-18 17:13:29 +0200

    Premiere route des community espaceco

--- src/Controller/EspaceCo/CommunityController.php
+++ src/Controller/EspaceCo/CommunityController.php
@@ -3,8 +3,10 @@
 namespace App\Controller\EspaceCo;
 
 use App\Controller\ApiControllerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route(
     '/api/espaceco/community',
@@ -14,4 +16,20 @@ use Symfony\Component\Routing\Attribute\Route;
 )]
 class CommunityController extends AbstractController implements ApiControllerInterface
 {
+    #[Route('/get', name: 'get', methods: ['GET'],
+    options: ['expose' => true],
+    condition: 'request.isXmlHttpRequest()')]
+    public function get(
+        #[MapQueryParameter] ?int $page = 1,
+        #[MapQueryParameter] ?int $limit = 10
+    ): JsonResponse
+    {
+        /* try {
+
+            return new JsonResponse($community);
+        } catch (ApiException $ex) {
+            throw new CartesApiException($ex->getMessage(), $ex->getStatusCode(), $ex->getDetails(), $ex);
+        } */
+        return new JsonResponse();
+    }
 }
