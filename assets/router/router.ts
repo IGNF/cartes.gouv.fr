c8759707ba042180fa09314f800a54e41e6ab678
Sylvain Lafay <sylvain.lafay@ign.fr>
2024-06-11 11:14:11 +0200

    feat(router): revue des paths

--- assets/router/router.ts
+++ assets/router/router.ts
@@ -16,7 +16,7 @@ const routeDefs = {
     about: defineRoute(`${appRoot}/a-propos`),
     documentation: defineRoute(`${appRoot}/documentation`),
     contact: defineRoute(`${appRoot}/nous-ecrire`),
-    contact_thanks: defineRoute(`${appRoot}/merci`),
+    contact_thanks: defineRoute(`${appRoot}/nous-ecrire/demande-envoyee`),
     news_list: defineRoute(`${appRoot}/actualites`),
     news_article: defineRoute(
         {
@@ -37,22 +37,22 @@ const routeDefs = {
     // routes protégées
     // utilisateur
     my_account: defineRoute(`${appRoot}/mon-compte`),
-    my_access_keys: defineRoute(`${appRoot}/mes-cles-acces`),
-    user_key_add: defineRoute(`${appRoot}/add-key`),
+    my_access_keys: defineRoute(`${appRoot}/mes-cles`),
+    user_key_add: defineRoute(`${appRoot}/mes-cles/ajout`),
     user_key_edit: defineRoute(
         {
             keyId: param.path.string,
         },
-        (p) => `${appRoot}/edit_key/${p.keyId}`
+        (p) => `${appRoot}/mes-cles/${p.keyId}/modification`
     ),
 
-    dashboard_pro: defineRoute(`${appRoot}/dashboard`),
+    dashboard_pro: defineRoute(`${appRoot}/tableau-de-bord`),
 
     // Demande de creation d'un datastore
-    datastore_create_request: defineRoute(`${appRoot}/datastores/create-request`),
+    datastore_create_request: defineRoute(`${appRoot}/entrepot/demande-de-creation`),
 
     // Demande pour rejoindre une communaute
-    join_community: defineRoute(`${appRoot}/join-community`),
+    join_community: defineRoute(`${appRoot}/rejoindre-des-communautes`),
 
     // Liste des membres d'une communaute
     members_list: defineRoute(
@@ -60,37 +60,37 @@ const routeDefs = {
             datastoreId: param.path.string,
             userId: param.query.optional.string,
         },
-        (p) => `${appRoot}/datastore/${p.datastoreId}/members`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/membres`
     ),
 
     accesses_request: defineRoute(
         {
             fileIdentifier: param.path.string,
         },
-        (p) => `${appRoot}/accesses-request/${p.fileIdentifier}`
+        (p) => `${appRoot}/demande-acces/${p.fileIdentifier}`
     ),
 
-    datastore_create_request_confirm: defineRoute(`${appRoot}/confirmation`),
+    datastore_create_request_confirm: defineRoute(`${appRoot}/demande-acces/demande-envoyee`),
 
     datastore_manage_storage: defineRoute(
         {
             datastoreId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/manage-storage`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/consommation`
     ),
 
     datastore_manage_permissions: defineRoute(
         {
             datastoreId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/manage-permissions`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/permissions`
     ),
 
     datastore_add_permission: defineRoute(
         {
             datastoreId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/add-permission`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/permissions/ajout`
     ),
 
     datastore_edit_permission: defineRoute(
@@ -98,7 +98,7 @@ const routeDefs = {
             datastoreId: param.path.string,
             permissionId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/edit-permission/${p.permissionId}`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/permissions/${p.permissionId}/modification`
     ),
 
     // fiche de données
@@ -106,21 +106,21 @@ const routeDefs = {
         {
             datastoreId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/datasheet`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/donnees`
     ),
     datastore_datasheet_upload: defineRoute(
         {
             datastoreId: param.path.string,
             datasheetName: param.query.optional.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/datasheet/upload`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/donnees/televersement`
     ),
     datastore_datasheet_upload_integration: defineRoute(
         {
             datastoreId: param.path.string,
             uploadId: param.query.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/datasheet/upload/integration`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/donnees/integration`
     ),
     datastore_datasheet_view: defineRoute(
         {
@@ -128,14 +128,14 @@ const routeDefs = {
             datasheetName: param.path.string,
             activeTab: param.query.optional.string.default("metadata"),
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/datasheet/${p.datasheetName}`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/donnees/${p.datasheetName}`
     ),
     datastore_stored_data_details: defineRoute(
         {
             datastoreId: param.path.string,
             storedDataId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/stored_data/${p.storedDataId}`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/donnees/${p.storedDataId}/details`
     ),
 
     // Creer et publier un service WFS
@@ -144,7 +144,7 @@ const routeDefs = {
             datastoreId: param.path.string,
             vectorDbId: param.query.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/wfs/new`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/wfs/ajout`
     ),
     // Modifier les infos d'un service WFS
     datastore_wfs_service_edit: defineRoute(
@@ -153,7 +153,7 @@ const routeDefs = {
             vectorDbId: param.query.string,
             offeringId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/wfs/${p.offeringId}/edit`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/wfs/${p.offeringId}/modification`
     ),
 
     // Creer et publier un service WMS-VECTEUR
@@ -162,7 +162,7 @@ const routeDefs = {
             datastoreId: param.path.string,
             vectorDbId: param.query.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/wms-vector/new`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/wms-vecteur/ajout`
     ),
     // Modifier les infos d'un service WMS-VECTEUR
     datastore_wms_vector_service_edit: defineRoute(
@@ -171,7 +171,7 @@ const routeDefs = {
             vectorDbId: param.query.string,
             offeringId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/wms-vector/${p.offeringId}/edit`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/wms-vecteur/${p.offeringId}/modification`
     ),
 
     // Création/génération d'une pyramide vecteur
@@ -181,7 +181,7 @@ const routeDefs = {
             vectorDbId: param.query.string,
             technicalName: param.query.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/pyramid-vector/new`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/pyramide-vecteur/ajout`
     ),
 
     // Publier une pyramide vecteur en tant que service TMS
@@ -190,7 +190,7 @@ const routeDefs = {
             datastoreId: param.path.string,
             pyramidId: param.query.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/tms/new`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/tms/ajout`
     ),
     // Modifier les infos d'un service WMS-VECTEUR
     datastore_pyramid_vector_tms_service_edit: defineRoute(
@@ -199,7 +199,7 @@ const routeDefs = {
             pyramidId: param.query.string,
             offeringId: param.path.string,
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/tms/${p.offeringId}/edit`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/tms/${p.offeringId}/modification`
     ),
 
     datastore_service_view: defineRoute(
@@ -209,7 +209,7 @@ const routeDefs = {
             datasheetName: param.query.string,
             activeTab: param.query.optional.string.default("diffuse"),
         },
-        (p) => `${appRoot}/datastores/${p.datastoreId}/service/${p.offeringId}`
+        (p) => `${appRoot}/entrepot/${p.datastoreId}/service/${p.offeringId}/visualisation`
     ),
 
     espaceco_community_list: defineRoute(
