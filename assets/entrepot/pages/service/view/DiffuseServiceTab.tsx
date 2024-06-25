430bf19c6e3cea5a3ebac75a41630eb9dea4d51f
Orka Arnest CRUZE <orka.cruze@ign.fr>
2024-06-25 18:31:22 +0200

    feat: suppr code html iframe, lien public carte et ajoute URL fichiers style #420

--- assets/entrepot/pages/service/view/DiffuseServiceTab.tsx
+++ assets/entrepot/pages/service/view/DiffuseServiceTab.tsx
@@ -1,13 +1,15 @@
 import { fr } from "@codegouvfr/react-dsfr";
-import { FC } from "react";
+import { FC, useMemo } from "react";
 
-import TextCopyToClipboard from "../../../../components/Utils/TextCopyToClipboard";
 import { Service } from "../../../../@types/app";
+import TextCopyToClipboard from "../../../../components/Utils/TextCopyToClipboard";
 
 type DiffuseServiceTabProps = {
     service?: Service;
 };
 const DiffuseServiceTab: FC<DiffuseServiceTabProps> = ({ service }) => {
+    const currentStyleLayers = useMemo(() => service?.configuration.styles?.find((style) => style.current === true)?.layers, [service?.configuration.styles]);
+
     return (
         <div className={fr.cx("fr-col-12")}>
             <div className={fr.cx("fr-grid-row")}>
@@ -15,16 +17,16 @@ const DiffuseServiceTab: FC<DiffuseServiceTabProps> = ({ service }) => {
             </div>
 
             {/* lien public vers la carte */}
-            <div className={fr.cx("fr-grid-row")}>
+            {/* <div className={fr.cx("fr-grid-row")}>
                 <strong>Lien public vers la carte</strong>
             </div>
-            <TextCopyToClipboard text={"http://www.ign.fr/geoplateforme"} className="fr-mb-4w" disabled />
+            <TextCopyToClipboard text={"http://www.ign.fr/geoplateforme"} className="fr-mb-4w" disabled /> */}
 
             {/* Code HTML de l'iframe */}
-            <div className={fr.cx("fr-grid-row")}>
+            {/* <div className={fr.cx("fr-grid-row")}>
                 <strong>{"Code HTML de l'iframe"}</strong>
             </div>
-            <TextCopyToClipboard text={"<iframe width='600' height='40..."} className="fr-mb-4w" disabled />
+            <TextCopyToClipboard text={"<iframe width='600' height='40..."} className="fr-mb-4w" disabled /> */}
 
             {/* Adresse du service de données */}
             <div className={fr.cx("fr-grid-row")}>
@@ -32,6 +34,21 @@ const DiffuseServiceTab: FC<DiffuseServiceTabProps> = ({ service }) => {
             </div>
 
             <TextCopyToClipboard text={service?.share_url ?? "Indisponible"} disabled={!service?.share_url} className="fr-mb-1w" />
+
+            <div className={fr.cx("fr-grid-row", "fr-mt-4v")}>
+                <strong>Adresse des fichiers de style courant</strong>
+            </div>
+            {currentStyleLayers?.map((layer) => (
+                <div key={layer.annexe_id} className={fr.cx("fr-mt-2v")}>
+                    <span>
+                        {(() => {
+                            const splitLayerName = layer.name?.split(":");
+                            return splitLayerName?.[1] ?? splitLayerName?.[0] ?? layer.name;
+                        })()}
+                    </span>
+                    <TextCopyToClipboard text={layer.url} successMessage="URL copiée" />
+                </div>
+            ))}
         </div>
     );
 };
