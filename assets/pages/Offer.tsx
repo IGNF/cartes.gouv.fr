cf5cebc1bfdc84819307747d8841d5277367be11
Orka Arnest CRUZE <orka.cruze@ign.fr>
2024-07-11 14:28:36 +0200

    feat: modifs page accueil et offre #464 #465

--- assets/pages/Offer.tsx
+++ assets/pages/Offer.tsx
@@ -4,6 +4,8 @@ import CallOut from "@codegouvfr/react-dsfr/CallOut";
 import AppLayout from "../components/Layout/AppLayout";
 import { routes } from "../router/router";
 
+import "../sass/pages/offer.scss";
+
 const Offer = () => {
     return (
         <AppLayout documentTitle="Offre">
@@ -22,31 +24,69 @@ const Offer = () => {
 
             <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters", "fr-mb-3w")}>
                 <div className={fr.cx("fr-col-12", "fr-col-md-6", "fr-col-lg-4")}>
-                    <CallOut title="Offre Essentielle" titleAs="h2" colorVariant="green-emeraude">
-                        <ul>
-                            <li>1 entrepôt</li>
-                            <li>500Go hébergés</li>
+                    <CallOut
+                        title="Offre Essentielle"
+                        titleAs="h2"
+                        colorVariant="green-emeraude"
+                        classes={{
+                            title: fr.cx("fr-mb-4v") + " frx-text--center",
+                            root: "frx-callout--offer-root",
+                            text: "frx-callout--offer-text",
+                        }}
+                    >
+                        <p className={fr.cx("fr-py-2v") + " frx-text--center frx-callout--offer-text-tagline"}>
+                            {
+                                "Une offre adaptée aux besoins les plus courants, avec l'ensemble des fonctionnalités et un premier niveau de stockage et de diffusion associé."
+                            }
+                        </p>
+
+                        <ul className="frx-callout--offer-text-list">
+                            <li>
+                                500 Go
+                                <br />
+                                Hébergés sur un entrepôt
+                            </li>
                             <li>50 couches de données</li>
-                            <li>1To max de données utilisées</li>
+                            <li>1 To max de trafic par an</li>
                             <li>Animation des communautés de contributeurs</li>
                             <li>Support par mail</li>
                         </ul>
-                        <p className={fr.cx("fr-text--sm", "fr-mt-4v")}>Gratuit</p>
+
+                        <p className={fr.cx("fr-text--xl", "fr-text--heavy") + " frx-text--center frx-callout--offer-text-price"}>Gratuit</p>
                     </CallOut>
                 </div>
 
                 <div className={fr.cx("fr-col-12", "fr-col-md-6", "fr-col-lg-4")}>
-                    <CallOut title="Offre Premium" titleAs="h2" colorVariant="brown-caramel">
-                        <ul>
-                            <li>Plusieurs entrepôts</li>
-                            <li>20To hébergés</li>
+                    <CallOut
+                        title="Offre Premium"
+                        titleAs="h2"
+                        colorVariant="brown-caramel"
+                        classes={{
+                            title: fr.cx("fr-mb-4v") + " frx-text--center",
+                            root: "frx-callout--offer-root",
+                            text: "frx-callout--offer-text",
+                        }}
+                    >
+                        <p className={fr.cx("fr-py-2v") + " frx-text--center frx-callout--offer-text-tagline"}>
+                            {"Une offre calibrée pour les volumes de données les plus importants tels que PCRS ou portail cartographique national."}
+                        </p>
+                        <ul className="frx-callout--offer-text-list">
+                            <li>
+                                20 To
+                                <br />
+                                Hébergés sur un ou plusieurs entrepôts
+                            </li>
                             <li>Couches de données illimitées</li>
-                            <li>Au delà de 1To de données utilisées (tarif dégressif selon volume)</li>
-                            <li>Animation de communautés contributeurs</li>
+                            <li>Au-delà de 1 To max de trafic par an</li>
+                            <li>Animation des communautés de contributeurs</li>
                             <li>Support par mail et formation</li>
                             <li>Participation à la gouvernance</li>
                         </ul>
-                        <p className={fr.cx("fr-text--sm", "fr-mt-4v")}>à partir de 10k€ par an</p>
+                        <p className={"frx-text--center frx-callout--offer-text-price"}>
+                            <p className={fr.cx("fr-text--sm")}>à partir de</p>
+                            <p className={fr.cx("fr-text--xl", "fr-text--heavy")}>10 K€ HT par an</p>
+                            <p className={fr.cx("fr-text--sm")}>(tarif dégressif selon le volume de trafic : de 3,3 k€ à 1,5 k€ par To et par an)</p>
+                        </p>
                     </CallOut>
                 </div>
             </div>
