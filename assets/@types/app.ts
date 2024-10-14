2f22f17599381a6c56ff4e1b732f9f4c860f8c46
Orka Arnest CRUZE <orka.cruze@ign.fr>
2024-10-14 19:41:46 +0200

    feat: fin de formulaire génération de pyramide raster #470

--- assets/@types/app.ts
+++ assets/@types/app.ts
@@ -126,6 +126,10 @@ export type PyramidRaster = StoredData & {
     type: StoredDataPrivateDetailResponseDtoTypeEnum.ROK4PYRAMIDRASTER;
     tags: {
         datasheet_name?: string;
+        upload_id?: string;
+        proc_int_id?: string;
+        vectordb_id?: string;
+        proc_pyr_creat_id?: string;
     };
 };
 
@@ -189,7 +193,12 @@ export type TmsMetadata = {
 };
 
 /** configuration & offerings */
-export type Configuration = ConfigurationDetailResponseDto & { styles?: CartesStyle[] };
+export type Configuration = ConfigurationDetailResponseDto & {
+    styles?: CartesStyle[];
+    tags: {
+        datasheet_name?: string;
+    };
+};
 
 export type Offering = OfferingDetailResponseDto;
 export { OfferingStatusEnum } from "./entrepot";
