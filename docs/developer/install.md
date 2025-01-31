969e1547bf3340a9e56c9a2e370df8d65d97c485
Orka Arnest CRUZE <33525693+ocruze@users.noreply.github.com>
2025-01-31 16:03:40 +0100

    build: passage à vite (#621)

--- docs/developer/install.md
+++ docs/developer/install.md
@@ -1,6 +1,6 @@
 # Installation et configuration
 
-Le portail est construit sur le framework PHP Symfony. Il nécessite l'installation d'un moteur php >=8.0 avec les extensions `xsl` et `intl`, composer et yarn.
+Le portail est construit sur le framework PHP Symfony. Il nécessite l'installation d'un moteur php >=8.2 avec les extensions `xsl` et `intl`, composer et yarn.
 
 1. Si nécessaire, s'assurer que le proxy est correctement configuré (par exemple au moyen des variables d'environnement `http_proxy` et `https_proxy`)
 
@@ -12,13 +12,16 @@ Le portail est construit sur le framework PHP Symfony. Il nécessite l'installat
 
 5. Installer les dépendances Javascript (`yarn install`)
 
-6. Compiler les assets (`yarn encore [dev|production]`)
+6. Compiler les assets (voir détails des commandes suivantes dans le fichier [package.json](./../../package.json)) :
+    - `yarn dev` : lancer le serveur de développement vite avec hot-reload, pour le développement en local
+    - `yarn build` : générer les assets en mode production
+    - `yarn build:dev` : générer les assets en mode développement
 
 ### Spécificités de l'installation avec Docker
 
--   Lancer les conteneurs docker : `docker compose -f docker-compose[.dev].yml up [-d] [--build]`
+- Lancer les conteneurs docker : `docker compose -f compose[.prod].yml up [-d] [--build]`
 
-> `-f` permet de spécifier le fichier docker-compose.yml à utiliser (il y a plusieurs fichiers, chaque fichier correspondant à un environnement)
+> `-f` permet de spécifier le fichier compose.yml à utiliser (il y a plusieurs fichiers, chaque fichier correspondant à un environnement)
 >
 > `-d` permet de créer le conteneur docker en mode "détaché"
 >
---
+++ vite.config.js
@@ -0,0 +1,62 @@
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
+import { configDotenv } from "dotenv";
+import { dirname, join, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import process from "process";
+import { defineConfig } from "vite";
+import run from "vite-plugin-run";
+import symfonyPlugin from "vite-plugin-symfony";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+configDotenv({
+    path: [resolve(__dirname, ".env.local")],
+});
+
+export default defineConfig({
+    server: {
+        // Required to listen on all interfaces
+        host: "0.0.0.0",
+        cors: true,
+        force: true,
+    },
+    plugins: [
+        react(), // if you're using React
+        symfonyPlugin({
+            viteDevServerHostname: "localhost",
+            refresh: true,
+            sriAlgorithm: "sha384",
+            debug: process.env.APP_ENV === "dev",
+            exposedEnvVars: ["APP_ENV", "APP_ROOT_URL"],
+        }),
+        run([
+            {
+                name: "fos-routing-js-dump",
+                run: ["php", "bin/console", "fos:js-routing:dump", "--target", "./var/cache/fosRoutes.json", "--format", "json"],
+                pattern: ["src/Controller/**.php"],
+            },
+        ]),
+    ],
+    base: process.env.ENCORE_PUBLIC_PATH ?? "/build/",
+    build: {
+        outDir: resolve(join(__dirname, "public", "build")),
+        rollupOptions: {
+            input: {
+                main: resolve(join(__dirname, "./assets", "main.tsx")),
+                dsfr: resolve(join(__dirname, "./node_modules", "@codegouvfr", "react-dsfr", "main.css")),
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@": resolve(join(__dirname, "assets")),
+        },
+    },
+    css: {
+        postcss: {
+            plugins: [autoprefixer()],
+        },
+    },
+});
