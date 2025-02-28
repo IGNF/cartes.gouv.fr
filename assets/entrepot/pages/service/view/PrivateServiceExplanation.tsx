c31b580e6a5466ee9a2c01193ea382498511d7d0
Sylvain Lafay <sylvain.lafay@ign.fr>
2025-02-28 10:05:45 +0100

    feat: modif texte et picto flux privés

--- assets/entrepot/pages/service/view/PrivateServiceExplanation.tsx
+++ assets/entrepot/pages/service/view/PrivateServiceExplanation.tsx
@@ -4,7 +4,7 @@ import { fr } from "@codegouvfr/react-dsfr";
 import { Button } from "@codegouvfr/react-dsfr/Button";
 
 import ovoidSvgUrl from "@codegouvfr/react-dsfr/dsfr/artwork/background/ovoid.svg";
-import locationFranceSvgUrl from "@codegouvfr/react-dsfr/dsfr/artwork/pictograms/map/location-france.svg";
+import padlockSvgUrl from "@codegouvfr/react-dsfr/dsfr/artwork/pictograms/system/padlock.svg?no-inline";
 
 type PrivateServiceExplanationProps = {
     datastoreId: string;
@@ -13,25 +13,27 @@ function PrivateServiceExplanation({ datastoreId }: PrivateServiceExplanationPro
     return (
         <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters", "fr-grid-row--center", "fr-grid-row--middle")}>
             <div className={fr.cx("fr-py-0", "fr-col-12", "fr-col-md-6")}>
-                <p className={fr.cx("fr-text--lead", "fr-mb-3w")}>Ce service est privé et ne peut donc pas être affiché sur cartes.gouv.fr.</p>
+                <h2>Service privé</h2>
+
+                <p className={fr.cx("fr-text--lead", "fr-mb-3w")}>Ce service est privé et ne peut être affiché sur cartes.gouv.fr.</p>
 
                 <p className={fr.cx("fr-text--sm")}>
-                    Vous pouvez utiliser un logiciel SIG (Système d’Information Géographique) pour le visualiser. Au préalable vous devez créer une clé d’accès
-                    ou modifier une de vos clés d’accès existantes.
+                    Vous pouvez visualiser ce service sous un logiciel SIG (Système d’Information Géographique) à l’aide d’une clé d’accès. Vous pouvez créer,
+                    modifier ou retrouver toutes vos clés d’accès depuis votre tableau de bord.
                 </p>
 
                 <p className={fr.cx("fr-text--sm", "fr-mb-5w")}>
-                    Pour donner accès à d’autres personnes, vous pouvez leur donner une permission individuelle ou donner une permission à une communauté dont
-                    elles sont membres. Elles pourront ensuite configurer leurs propres clés.
+                    Si vous souhaitez partager ce service avec d’autres personnes, il est nécessaire de configurer au préalable les permissions afin qu’elles
+                    puissent créer leur propre clé.
                 </p>
 
                 <ul className={fr.cx("fr-btns-group", "fr-btns-group--inline-md")}>
                     <li>
-                        <Button {...routes.my_access_keys().link}>Ajouter ou modifier une clé d’accès</Button>
+                        <Button {...routes.my_access_keys().link}>Voir mes clé d’accès</Button>
                     </li>
                     <li>
                         <Button {...routes.datastore_add_permission({ datastoreId }).link} className={fr.cx("fr-btn--secondary")}>
-                            Configurer des permissions
+                            Configurer les permissions
                         </Button>
                     </li>
                 </ul>
@@ -48,9 +50,9 @@ function PrivateServiceExplanation({ datastoreId }: PrivateServiceExplanationPro
                     <use className={fr.cx("fr-artwork-motif")} href={`${ovoidSvgUrl}#artwork-motif`} />
                     <use className={fr.cx("fr-artwork-background")} href={`${ovoidSvgUrl}#artwork-background`} />
                     <g transform="translate(40, 60)">
-                        <use className={fr.cx("fr-artwork-decorative")} href={`${locationFranceSvgUrl}#artwork-decorative`} />
-                        <use className={fr.cx("fr-artwork-minor")} href={`${locationFranceSvgUrl}#artwork-minor`} />
-                        <use className={fr.cx("fr-artwork-major")} href={`${locationFranceSvgUrl}#artwork-major`} />
+                        <use className={fr.cx("fr-artwork-decorative")} href={`${padlockSvgUrl}#artwork-decorative`} />
+                        <use className={fr.cx("fr-artwork-minor")} href={`${padlockSvgUrl}#artwork-minor`} />
+                        <use className={fr.cx("fr-artwork-major")} href={`${padlockSvgUrl}#artwork-major`} />
                     </g>
                 </svg>
             </div>
