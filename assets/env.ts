39b3193b9080578a541de93baf208f9371592903
Tony CABAYE <tony.cabaye@smile.fr>
2025-04-09 15:50:44 +0200

    feat: add configurable technical name for config datastore + clear RSS feed cache + manage annexe add

--- assets/env.ts
+++ assets/env.ts
@@ -2,5 +2,6 @@ const rootDataset = (document.getElementById("root") as HTMLDivElement)?.dataset
 
 export const espaceCoUrl = rootDataset?.["apiEspacecoUrl"] ?? null;
 export const catalogueUrl = rootDataset?.["catalogueUrl"] ?? "/catalogue";
-export const communityId = rootDataset?.["configCommunityId"] ?? null;
+export const configCommunityId = rootDataset?.["configCommunityId"] ?? null;
+export const configTechnicalName = rootDataset?.["configTechnicalName"] ?? null;
 export const annexesUrl = rootDataset?.["annexesUrl"] ?? "https://data.geopf.fr/annexes";
