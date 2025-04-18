1b6133b1b88e1e149a22de669011cbacf57464ec
Tony CABAYE <tony.cabaye@smile.fr>
2025-04-18 11:42:24 +0200

    feat: apply dsfr styles

--- assets/components/Utils/GeostylerEditor.tsx
+++ assets/components/Utils/GeostylerEditor.tsx
@@ -6,7 +6,7 @@ import { Style as GsStyle } from "geostyler-style";
 import { FC } from "react";
 import { tss } from "tss-react";
 
-// import DsfrAntdConfig from "../Provider/DsfrAntdConfig";
+import DsfrAntdConfig from "../Provider/DsfrAntdConfig";
 
 export const sld100Parser = new SldStyleParser({
     builderOptions: {
@@ -36,40 +36,42 @@ const GeostylerEditor: FC<GeostylerEditorProps> = (props) => {
 
     return (
         <GeoStylerContext.Provider value={ctx}>
-            <Tabs
-                label="Editeur de style"
-                tabs={[
-                    {
-                        label: "Tableau",
-                        content: (
-                            <div className={fr.cx("fr-grid-row", "fr-my-2w")}>
-                                <div className={fr.cx("fr-col")}>
-                                    <Style style={value} onStyleChange={onChange} />
-                                </div>
-                            </div>
-                        ),
-                    },
-                    {
-                        label: "Editeur de code",
-                        content: (
-                            <div className={fr.cx("fr-grid-row", "fr-my-2w")}>
-                                <div className={fr.cx("fr-col")}>
-                                    <div className={classes.editor}>
-                                        <CodeEditor
-                                            style={value}
-                                            onStyleChange={onChange}
-                                            defaultParser={sld100Parser}
-                                            showCopyButton={true}
-                                            showSaveButton={true}
-                                            parsers={[sld100Parser]}
-                                        />
+            <DsfrAntdConfig>
+                <Tabs
+                    label="Editeur de style"
+                    tabs={[
+                        {
+                            label: "Tableau",
+                            content: (
+                                <div className={fr.cx("fr-grid-row", "fr-my-2w")}>
+                                    <div className={fr.cx("fr-col")}>
+                                        <Style style={value} onStyleChange={onChange} />
                                     </div>
                                 </div>
-                            </div>
-                        ),
-                    },
-                ]}
-            />
+                            ),
+                        },
+                        {
+                            label: "Editeur de code",
+                            content: (
+                                <div className={fr.cx("fr-grid-row", "fr-my-2w")}>
+                                    <div className={fr.cx("fr-col")}>
+                                        <div className={classes.editor}>
+                                            <CodeEditor
+                                                style={value}
+                                                onStyleChange={onChange}
+                                                defaultParser={sld100Parser}
+                                                showCopyButton={true}
+                                                showSaveButton={true}
+                                                parsers={[sld100Parser]}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                            ),
+                        },
+                    ]}
+                />
+            </DsfrAntdConfig>
         </GeoStylerContext.Provider>
     );
 };
