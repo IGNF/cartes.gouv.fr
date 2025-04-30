bb18431ca852d0454a3c48497e1e93acb14a4aef
Tony CABAYE <tony.cabaye@smile.fr>
2025-04-30 09:39:03 +0200

    fix: lint error

--- assets/validations/StyleValidator.ts
+++ assets/validations/StyleValidator.ts
@@ -10,16 +10,7 @@ class StyleValidator {
         this.service = service;
         this.format = format;
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async validate(value: string | undefined, ctx: TestContext): Promise<ValidationError | boolean> {
-        // const extension = this.format === "mapbox" ? "json" : this.format;
-
-        // const file = files[0];
-        // if (file !== undefined && getFileExtension(file?.name)?.toLowerCase() !== extension) {
-        //     return ctx.createError({ message: "unaccepted_extension" });
-        // }
-
         if (typeof value !== "string" || !value) {
             return ctx.createError({ message: "no_file_provided" });
         }
