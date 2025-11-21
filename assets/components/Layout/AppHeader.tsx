de3b02b796f14461eaff64274b96e6b4375f84a8
Orka Arnest CRUZE <orka.cruze@ign.fr>
2025-11-21 17:34:48 +0100

    feat: refonte header avec Popper sans mui/Menu #829

--- assets/components/Layout/AppHeader.tsx
+++ assets/components/Layout/AppHeader.tsx
@@ -1,16 +1,10 @@
 import Badge from "@codegouvfr/react-dsfr/Badge";
-import { fr } from "@codegouvfr/react-dsfr";
-import Header, { HeaderProps } from "@codegouvfr/react-dsfr/Header";
+import Header from "@codegouvfr/react-dsfr/Header";
 import { MainNavigationProps } from "@codegouvfr/react-dsfr/MainNavigation/MainNavigation";
 import { FC, memo } from "react";
 
-import { catalogueUrl } from "@/env";
-// import { useLang } from "../../i18n/i18n";
-import SymfonyRouting from "../../modules/Routing";
-import { groups, routes, useRoute } from "../../router/router";
-import HeaderMenu from "./Header/HeaderMenu";
-import { useAuthStore } from "../../stores/AuthStore";
-// import LanguageSelector from "../Utils/LanguageSelector";
+import { routes } from "../../router/router";
+import { HeaderMenuHelp, HeaderMenuServices, HeaderMenuUser } from "./Header/HeaderMenus";
 
 import "../../sass/components/header.scss";
 
@@ -18,151 +12,6 @@ type AppHeaderProps = {
     navItems?: MainNavigationProps.Item[];
 };
 const AppHeader: FC<AppHeaderProps> = ({ navItems = [] }) => {
-    const user = useAuthStore((state) => state.user);
-    // const { lang, setLang } = useLang();
-    const route = useRoute();
-
-    const quickAccessItems: (HeaderProps.QuickAccessItem | JSX.Element | null)[] = [];
-
-    const geoportailQuickAccessItem: HeaderProps.QuickAccessItem = {
-        iconId: "fr-icon-arrow-right-line",
-        linkProps: {
-            href: "https://www.geoportail.gouv.fr/carte",
-            className: "fr-btn--icon-right",
-            target: "_blank",
-            rel: "noreferrer",
-            title: "Accéder au Géoportail - ouvre une nouvelle fenêtre",
-        },
-        text: "Accéder au Géoportail",
-    };
-
-    const catalogueQuickAccessItem: HeaderProps.QuickAccessItem = {
-        iconId: "fr-icon-arrow-right-line",
-        linkProps: {
-            href: catalogueUrl,
-            className: "fr-btn--icon-right",
-            target: "_blank",
-            rel: "noreferrer",
-            title: "Catalogue - ouvre une nouvelle fenêtre",
-        },
-        text: "Catalogue",
-    };
-
-    if (!user) {
-        // utilisateur n'est pas connecté
-        quickAccessItems.push(geoportailQuickAccessItem);
-
-        quickAccessItems.push({
-            iconId: "fr-icon-account-fill",
-            linkProps: {
-                href: SymfonyRouting.generate("cartesgouvfr_security_login"),
-            },
-            text: "Se connecter",
-        });
-    } else {
-        // utilisateur est connecté
-        if (route.name === false || groups.public.has(route)) {
-            // on garde le lien vers le géoportail sur les pages également accessibles publiquement
-            quickAccessItems.push(geoportailQuickAccessItem);
-        } else {
-            // on met plutôt le lien catalogue sur les pages accessibles uniquement connecté
-            quickAccessItems.push(catalogueQuickAccessItem);
-        }
-
-        let btnMyAccountText = `${user.first_name ?? ""} ${user.last_name ?? ""}`;
-        if (btnMyAccountText.replace(/\s+/g, "") === "") {
-            btnMyAccountText = user.user_name;
-        }
-
-        quickAccessItems.push({
-            iconId: "fr-icon-account-fill",
-            linkProps: routes.dashboard_pro().link,
-            text: btnMyAccountText.trim(),
-        });
-        quickAccessItems.push({
-            iconId: "fr-icon-logout-box-r-line",
-            linkProps: {
-                href: SymfonyRouting.generate("cartesgouvfr_security_logout"),
-            },
-            text: "Se déconnecter",
-        });
-        quickAccessItems.push(
-            <HeaderMenu
-                openButtonProps={{
-                    children: "Services",
-                    iconId: "fr-icon-grid-fill",
-                }}
-                actionButtonProps={{
-                    children: "Découvrir cartes.gouv",
-                    linkProps: {
-                        target: "_blank",
-                        rel: "noreferrer",
-                        href: "https://www.cartes.gouv.fr",
-                    },
-                }}
-                items={[
-                    {
-                        children: (
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "column",
-                                }}
-                            >
-                                <strong>Nom utilisateur</strong>
-                                <span
-                                    className={fr.cx("fr-text--xs", "fr-m-0")}
-                                    style={{
-                                        color: fr.colors.decisions.text.mention.grey.default,
-                                    }}
-                                >
-                                    adresseutilisateur@email.com
-                                </span>
-                            </div>
-                        ),
-                    },
-                    {
-                        iconId: "fr-icon-road-map-line",
-                        children: "Explorer les cartes",
-                        linkProps: {
-                            href: "#",
-                        },
-                    },
-                    {
-                        iconId: "fr-icon-search-line",
-                        children: "Rechercher une donnée",
-                        linkProps: {
-                            href: "#",
-                        },
-                    },
-                    {
-                        iconId: "fr-icon-database-line",
-                        children: "Publier une donnée",
-                        linkProps: {
-                            href: "#",
-                        },
-                    },
-                    {
-                        iconId: "fr-icon-brush-line",
-                        children: (
-                            <>
-                                Créer une carte{" "}
-                                <Badge severity="success" className={"fr-ml-auto"}>
-                                    Bêta
-                                </Badge>
-                            </>
-                        ),
-                        linkProps: {
-                            href: "#",
-                        },
-                    },
-                ]}
-            />
-        );
-    }
-
-    // quickAccessItems.push(<LanguageSelector lang={lang} setLang={setLang} />);
-
     return (
         <Header
             brandTop={
@@ -185,7 +34,11 @@ const AppHeader: FC<AppHeaderProps> = ({ navItems = [] }) => {
                 </>
             }
             serviceTagline="Le service public des cartes et données du territoire"
-            quickAccessItems={quickAccessItems}
+            quickAccessItems={[
+                <HeaderMenuHelp key="header-menu-help" />,
+                <HeaderMenuServices key="header-menu-services" />,
+                <HeaderMenuUser key="header-menu-user" />,
+            ]}
             // renderSearchInput={({ className, id, name, placeholder, type }) => (
             //     <input className={className} id={id} name={name} placeholder={placeholder} type={type} />
             // )}
---
+++ assets/components/Layout/Header/HeaderMenus.tsx
@@ -0,0 +1,150 @@
+import { fr } from "@codegouvfr/react-dsfr";
+import Button from "@codegouvfr/react-dsfr/Button";
+
+import SymfonyRouting from "@/modules/Routing";
+import { externalLink, externalUrls } from "@/router/externalUrls";
+import { routes } from "@/router/router";
+import { useAuthStore } from "@/stores/AuthStore";
+import HeaderMenu from "./HeaderMenu";
+
+export function HeaderMenuHelp() {
+    return (
+        <HeaderMenu
+            openButtonProps={{
+                children: "Aide",
+                iconId: "fr-icon-question-fill",
+            }}
+            items={[
+                {
+                    iconId: "fr-icon-question-mark",
+                    children: "Questions fréquentes",
+                    linkProps: externalLink("documentation", "Questions fréquentes"),
+                },
+                {
+                    iconId: "fr-icon-book-2-line",
+                    children: "Guide d'utilisation",
+                    linkProps: externalLink("documentationProducerGuide", "Guide d'utilisation"),
+                },
+                {
+                    iconId: "fr-icon-mail-line",
+                    children: "Nous contacter",
+                    linkProps: externalLink("contact_us", "Nous contacter"),
+                },
+            ]}
+        />
+    );
+}
+
+export function HeaderMenuServices() {
+    return (
+        <HeaderMenu
+            openButtonProps={{
+                children: "Services",
+                iconId: "fr-icon-grid-fill",
+            }}
+            items={[
+                {
+                    iconId: "fr-icon-road-map-line",
+                    children: "Explorer les cartes",
+                    linkProps: { href: externalUrls.maps },
+                },
+                {
+                    iconId: "fr-icon-search-line",
+                    children: "Rechercher une donnée",
+                    linkProps: { href: externalUrls.catalogue },
+                },
+                {
+                    iconId: "fr-icon-database-line",
+                    children: "Publier une donnée",
+                    linkProps: routes.dashboard_pro().link,
+                },
+                // {
+                //     iconId: "fr-icon-brush-line",
+                //     children: (
+                //         <>
+                //             Créer une carte{" "}
+                //             <Badge severity="success" className={"fr-ml-auto"}>
+                //                 Bêta
+                //             </Badge>
+                //         </>
+                //     ),
+                //     linkProps: { href: externalUrls.create_map },
+                // },
+            ]}
+            actionButtonProps={{
+                children: "Découvrir cartes.gouv",
+                linkProps: externalLink("discover_cartesgouvfr", "Découvrir cartes.gouv"),
+            }}
+        />
+    );
+}
+
+export function HeaderMenuUser() {
+    const user = useAuthStore((state) => state.user);
+
+    if (!user) {
+        return (
+            <Button
+                iconId="fr-icon-account-fill"
+                linkProps={{
+                    href: SymfonyRouting.generate("cartesgouvfr_security_login"),
+                }}
+            >
+                Se connecter
+            </Button>
+        );
+    }
+
+    let userDisplayName = `${user.first_name ?? ""} ${user.last_name ?? ""}`;
+    if (userDisplayName.replace(/\s+/g, "") === "") {
+        userDisplayName = user.user_name;
+    }
+
+    return (
+        <HeaderMenu
+            openButtonProps={{
+                children: "Mon espace",
+                iconId: "fr-icon-account-fill",
+            }}
+            items={[
+                {
+                    children: (
+                        <div
+                            style={{
+                                display: "flex",
+                                flexDirection: "column",
+                            }}
+                        >
+                            <strong>{userDisplayName}</strong>
+                            <span
+                                className={fr.cx("fr-text--xs", "fr-m-0")}
+                                style={{
+                                    color: fr.colors.decisions.text.mention.grey.default,
+                                }}
+                            >
+                                {user.email}
+                            </span>
+                        </div>
+                    ),
+                },
+                {
+                    children: "Tableau de bord",
+                    iconId: "fr-icon-dashboard-3-line",
+                    linkProps: routes.dashboard_pro().link,
+                },
+                {
+                    children: "Mon compte",
+                    iconId: "fr-icon-user-line",
+                    linkProps: routes.my_account().link,
+                },
+            ]}
+            actionButtonProps={{
+                children: "Se déconnecter",
+                iconId: "fr-icon-logout-box-r-line",
+                linkProps: {
+                    href: SymfonyRouting.generate("cartesgouvfr_security_logout"),
+                },
+            }}
+        />
+    );
+}
