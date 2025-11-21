b0f938a6d556793383fa747bdac00c65af3c28d0
Orka Arnest CRUZE <orka.cruze@ign.fr>
2025-11-21 17:34:48 +0100

    feat: refonte header avec Popper sans mui/Menu #829

--- assets/components/Layout/Header/HeaderMenu.tsx
+++ assets/components/Layout/Header/HeaderMenu.tsx
@@ -2,36 +2,22 @@ import { fr, FrIconClassName, RiIconClassName } from "@codegouvfr/react-dsfr";
 import Button, { ButtonProps } from "@codegouvfr/react-dsfr/Button";
 import { getLink, RegisteredLinkProps } from "@codegouvfr/react-dsfr/link";
 import MuiDsfrThemeProvider from "@codegouvfr/react-dsfr/mui";
-import { Menu, MenuItem } from "@mui/material";
+import { ClickAwayListener, Grow, Paper, Popper } from "@mui/material";
 import { MouseEvent, PropsWithChildren, useId, useState } from "react";
 import { tss } from "tss-react";
 
-type ItemCommon = PropsWithChildren<{
+type Item = PropsWithChildren<{
     iconId?: FrIconClassName | RiIconClassName;
-    disabled?: boolean;
-}>;
-
-type ItemWithOnClick = {
-    onClick?: React.MouseEventHandler<HTMLElement>;
-    linkProps?: never;
-};
-
-type ItemWithLinkProps = {
-    onClick?: never;
     linkProps?: RegisteredLinkProps;
-};
-
-export type Item = ItemCommon &
-    // onClick et linkProps sont mutuellement exclusifs
-    (ItemWithOnClick | ItemWithLinkProps);
+}>;
 
 type HeaderMenuProps = {
     openButtonProps: Omit<ButtonProps, "linkProps" | "onClick" | "type">;
     actionButtonProps?: ButtonProps;
-    items: Item[];
+    items?: Item[];
     disabled?: boolean;
 };
-export default function HeaderMenu({ openButtonProps: openButtonProps, actionButtonProps, items, disabled }: HeaderMenuProps) {
+export default function HeaderMenu({ openButtonProps, actionButtonProps, items, disabled }: HeaderMenuProps) {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
     const isMenuOpen = Boolean(anchorEl);
 
@@ -39,7 +25,11 @@ export default function HeaderMenu({ openButtonProps: openButtonProps, actionBut
     const menuId = useId();
 
     const handleBtnOpenClick = (e: MouseEvent<HTMLElement>) => {
-        setAnchorEl(e.currentTarget);
+        if (anchorEl) {
+            setAnchorEl(null);
+        } else {
+            setAnchorEl(e.currentTarget);
+        }
     };
 
     const handleClose = () => {
@@ -72,99 +62,89 @@ export default function HeaderMenu({ openButtonProps: openButtonProps, actionBut
                 <span className={fr.cx("fr-icon--sm", "ri-arrow-down-s-line", "fr-ml-2v")} aria-hidden="true" />
             </Button>
             <MuiDsfrThemeProvider>
-                <Menu
-                    id={menuId}
-                    slotProps={{
-                        list: {
-                            "aria-labelledby": openBtnId,
-                            className: cx(classes.list, classes.noMarginPadding),
-                        },
-                        root: {
-                            className: classes.noMarginPadding,
-                        },
-                        paper: {
-                            className: cx(classes.paper, classes.noMarginPadding),
-                        },
-                    }}
-                    anchorEl={anchorEl}
-                    open={isMenuOpen}
-                    onClose={handleClose}
-                    elevation={0}
-                    anchorOrigin={{
-                        vertical: "bottom",
-                        horizontal: "right",
-                    }}
-                    transformOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                    }}
-                >
-                    {items.map(({ onClick, linkProps, disabled, iconId, children }, i) => {
-                        const isUnclickable = !onClick && !linkProps;
-                        const itemContent = (
-                            <MenuItem
-                                key={i}
-                                className={cx(classes.menuItem, isUnclickable && classes.menuItemUnclickable, fr.cx("fr-text--sm"))}
-                                disableRipple
-                                onClick={
-                                    onClick
-                                        ? (e: MouseEvent<HTMLElement>) => {
-                                              onClick?.(e);
-                                              handleClose();
-                                          }
-                                        : undefined
-                                }
-                                disabled={disabled}
-                            >
-                                {iconId && <span className={fr.cx("fr-icon--sm", iconId)} />}
-                                {children}
-                            </MenuItem>
-                        );
+                <Popper open={isMenuOpen} anchorEl={anchorEl} placement="bottom-end" disablePortal={false} sx={{ zIndex: 1500 }} transition={true}>
+                    {({ TransitionProps }) => (
+                        <ClickAwayListener onClickAway={handleClose}>
+                            <Grow {...TransitionProps} style={{ transformOrigin: "top right" }}>
+                                <Paper className={cx(classes.paper)}>
+                                    <ul className={cx(fr.cx("fr-raw-list"))}>
+                                        {items?.map(({ linkProps, iconId, children }, i) => {
+                                            const content = (
+                                                <>
+                                                    {iconId && <span className={fr.cx("fr-icon--sm", iconId)} />}
+                                                    {children}
+                                                </>
+                                            );
 
-                        if (linkProps && !disabled) {
-                            return (
-                                <Link key={i} {...linkProps}>
-                                    {itemContent}
-                                </Link>
-                            );
-                        }
+                                            const handleLinkClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+                                                handleClose();
+                                                linkProps?.onClick?.(e);
+                                            };
 
-                        return itemContent;
-                    })}
+                                            return (
+                                                <li key={i} className={cx(classes.menuItem, !linkProps && classes.menuItemUnclickable)}>
+                                                    {linkProps ? (
+                                                        <Link {...linkProps} className={cx(fr.cx("fr-text--sm"))} onClick={handleLinkClick}>
+                                                            {content}
+                                                        </Link>
+                                                    ) : (
+                                                        <span className={cx(fr.cx("fr-text--sm"))}>{content}</span>
+                                                    )}
+                                                </li>
+                                            );
+                                        })}
+                                    </ul>
 
-                    {actionButtonProps !== undefined && (
-                        <Button
-                            {...actionButtonProps}
-                            priority="tertiary"
-                            className={cx(classes.actionButton, fr.cx("fr-m-4v"), actionButtonProps.className)}
-                        />
+                                    {actionButtonProps !== undefined && (
+                                        <Button
+                                            {...actionButtonProps}
+                                            priority="tertiary"
+                                            size="small"
+                                            className={cx(classes.actionButton, fr.cx("fr-m-4v"), actionButtonProps.className)}
+                                        />
+                                    )}
+                                </Paper>
+                            </Grow>
+                        </ClickAwayListener>
                     )}
-                </Menu>
+                </Popper>
             </MuiDsfrThemeProvider>
         </>
     );
 }
 
 const useStyles = tss.withName({ HeaderMenu }).create({
-    noMarginPadding: {
-        padding: 0,
-        margin: 0,
-    },
     paper: {
         display: "flex",
-        flexDirection: "row",
+        flexDirection: "column",
         justifyContent: "space-between",
         filter: "drop-shadow(var(--overlap-shadow))",
-    },
-    list: {
         width: "18rem",
     },
     menuItem: {
-        display: "flex",
-        gap: fr.spacing("2v"),
-        padding: `${fr.spacing("3v")} ${fr.spacing("4v")}`,
-        backgroundColor: "white",
         boxShadow: `0 -1px 0 0 ${fr.colors.decisions.border.default.grey.default}`,
+        padding: 0,
+        margin: 0,
+
+        "&:hover": {
+            backgroundColor: fr.colors.decisions.background.default.grey.hover,
+        },
+
+        "& a, & div": {
+            display: "flex",
+            gap: fr.spacing("2v"),
+            margin: 0,
+            padding: `${fr.spacing("3v")} ${fr.spacing("4v")}`,
+            textDecoration: "none",
+            boxShadow: "none",
+            backgroundImage: "none",
+        },
+        "& a": {
+            alignItems: "center",
+        },
+        "& a::after": {
+            marginLeft: "auto",
+        },
     },
     menuItemUnclickable: {
         cursor: "default",
@@ -177,10 +157,10 @@ const useStyles = tss.withName({ HeaderMenu }).create({
             backgroundColor: fr.colors.decisions.background.open.blueFrance.default,
         },
         "& span": {
-            transition: "transform 0.2s",
+            transition: "transform 0.3s",
         },
         '&[aria-expanded="true"] span': {
-            transform: "rotate(180deg)",
+            transform: "rotate(-180deg)",
         },
     },
     actionButton: {
