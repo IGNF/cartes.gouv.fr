38f79ddc9ff0fbbc378fba8f3dc30cc20d513063
Orka Arnest CRUZE <orka.cruze@ign.fr>
2025-12-11 16:11:09 +0100

    chore(deps): upgrade deps php et js

--- assets/main.tsx
+++ assets/main.tsx
@@ -2,24 +2,12 @@ import { startReactDsfr } from "@codegouvfr/react-dsfr/spa";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { mountStoreDevtool } from "simple-zustand-devtools";
 
 import App from "@/App";
-// import { useApiEspaceCoStore } from "./stores/ApiEspaceCoStore";
-// import { useAuthStore } from "./stores/AuthStore";
-// import { useSnackbarStore } from "./stores/SnackbarStore";
-
-import "ol/ol.css";
 
 // en prod
 if (import.meta.env?.APP_ENV?.toLowerCase() === "prod") {
     disableReactDevTools();
-}
-// en dev/qualif
-else {
-    // mountStoreDevtool("AuthStore", useAuthStore);
-    // mountStoreDevtool("ApiEspaceCoStore", useApiEspaceCoStore);
-    // mountStoreDevtool("SnackbarStore", useSnackbarStore);
 }
 
 startReactDsfr({ defaultColorScheme: "light" });
