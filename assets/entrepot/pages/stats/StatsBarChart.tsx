24e7d90b20601efc8c8deacee640e0d1747fc4c8
Orka Arnest CRUZE <orka.cruze@ign.fr>
2026-04-16 11:34:57 +0200

    chore: simplification de code

--- assets/entrepot/pages/stats/StatsBarChart.tsx
+++ assets/entrepot/pages/stats/StatsBarChart.tsx
@@ -1,37 +1,19 @@
 import { fr } from "@codegouvfr/react-dsfr";
-import { useMemo } from "react";
 
 import { HitStatisticsDto, StatsAggregation, StatsType } from "@/@types/stats";
-import LoadingText from "@/components/Utils/LoadingText";
 import { IUseBarChartOptions } from "@/hooks/useBarChart";
 import Chart from "@/pages/Chart";
 import { niceBytes } from "@/utils";
 
 interface StatsBarChartProps {
-    stats?: HitStatisticsDto;
+    stats: HitStatisticsDto;
     startDate?: Date;
     endDate?: Date;
 }
 export default function StatsBarChart(props: StatsBarChartProps) {
     const { stats, startDate, endDate } = props;
 
-    const dataTransferChartProps: IUseBarChartOptions | null = useMemo(() => {
-        if (!stats) {
-            return null;
-        }
-        return { aggregation: StatsAggregation.DAY, data: stats, type: StatsType.DATA_TRANSFERT, startDate, endDate };
-    }, [stats, startDate, endDate]);
-
-    const hitsChartProps: IUseBarChartOptions | null = useMemo(() => {
-        if (!stats) {
-            return null;
-        }
-        return { aggregation: StatsAggregation.DAY, data: stats, type: StatsType.HITS, startDate, endDate };
-    }, [stats, startDate, endDate]);
-
-    if (!stats) {
-        return <div>Pas de données</div>;
-    }
+    const chartProps: IUseBarChartOptions = { aggregation: StatsAggregation.DAY, data: stats, type: StatsType.DATA_TRANSFERT, startDate, endDate };
 
     if (!stats.total?.begin_date) {
         return <div>Aucune donnée trouvée</div>;
@@ -50,16 +32,8 @@ export default function StatsBarChart(props: StatsBarChartProps) {
                     </li>
                 </ul>
 
-                {!dataTransferChartProps ? (
-                    <LoadingText className={fr.cx("fr-ml-2w")} withSpinnerIcon as="p" />
-                ) : (
-                    <Chart {...dataTransferChartProps} name={["Volume de données transférées"]} />
-                )}
-                {!hitsChartProps ? (
-                    <LoadingText className={fr.cx("fr-ml-2w")} withSpinnerIcon as="p" />
-                ) : (
-                    <Chart {...hitsChartProps} name={["Nombre de hits"]} color={["blue-cumulus"]} />
-                )}
+                <Chart {...chartProps} type={StatsType.DATA_TRANSFERT} name={["Volume de données transférées"]} />
+                <Chart {...chartProps} type={StatsType.HITS} name={["Nombre de hits"]} color={["blue-cumulus"]} />
             </div>
         </div>
     );
