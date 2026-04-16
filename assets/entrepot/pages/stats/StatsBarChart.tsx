c59063f47fe25a5790f777831c7a59eddd42ff2b
Orka Arnest CRUZE <orka.cruze@ign.fr>
2026-04-16 11:14:30 +0200

    feat: 2 diagrammes data transf et hits, affichage des totaux

--- assets/entrepot/pages/stats/StatsBarChart.tsx
+++ assets/entrepot/pages/stats/StatsBarChart.tsx
@@ -1,11 +1,11 @@
 import { fr } from "@codegouvfr/react-dsfr";
-import Select from "@codegouvfr/react-dsfr/SelectNext";
-import { useMemo, useState } from "react";
+import { useMemo } from "react";
 
 import { HitStatisticsDto, StatsAggregation, StatsType } from "@/@types/stats";
 import LoadingText from "@/components/Utils/LoadingText";
 import { IUseBarChartOptions } from "@/hooks/useBarChart";
 import Chart from "@/pages/Chart";
+import { niceBytes } from "@/utils";
 
 interface StatsBarChartProps {
     stats?: HitStatisticsDto;
@@ -15,38 +15,52 @@ interface StatsBarChartProps {
 export default function StatsBarChart(props: StatsBarChartProps) {
     const { stats, startDate, endDate } = props;
 
-    const [dataType, setDataType] = useState(StatsType.DATA_TRANSFERT);
-
-    const chartProps: IUseBarChartOptions | null = useMemo(() => {
+    const dataTransferChartProps: IUseBarChartOptions | null = useMemo(() => {
         if (!stats) {
             return null;
         }
-        return { aggregation: StatsAggregation.DAY, data: stats, type: dataType, startDate, endDate };
-    }, [stats, dataType, startDate, endDate]);
+        return { aggregation: StatsAggregation.DAY, data: stats, type: StatsType.DATA_TRANSFERT, startDate, endDate };
+    }, [stats, startDate, endDate]);
+
+    const hitsChartProps: IUseBarChartOptions | null = useMemo(() => {
+        if (!stats) {
+            return null;
+        }
+        return { aggregation: StatsAggregation.DAY, data: stats, type: StatsType.HITS, startDate, endDate };
+    }, [stats, startDate, endDate]);
 
     if (!stats) {
         return <div>Pas de données</div>;
     }
 
-    return (
-        <div>
-            <Select
-                label="Type de données"
-                options={[
-                    { label: "Volume de données transférées", value: StatsType.DATA_TRANSFERT },
-                    { label: "Nombre de hits", value: StatsType.HITS },
-                ]}
-                nativeSelectProps={{
-                    value: dataType,
-                    onChange: (e) => setDataType(e.currentTarget.value as StatsType),
-                }}
-            />
+    if (!stats.total?.begin_date) {
+        return <div>Aucune donnée trouvée</div>;
+    }
 
-            {!chartProps ? (
-                <LoadingText className={fr.cx("fr-ml-2w")} withSpinnerIcon as="p" />
-            ) : (
-                <Chart {...chartProps} name={[dataType === StatsType.DATA_TRANSFERT ? "Volume de données transférées" : "Nombre de hits"]} />
-            )}
+    return (
+        <div className={fr.cx("fr-grid-row", "fr-grid-row--gutters")}>
+            <div className={fr.cx("fr-col")}>
+                <ul className={fr.cx("fr-raw-list")}>
+                    <li>
+                        <strong>Volume de données transférées</strong> :{" "}
+                        {stats.total?.data_transfer ? niceBytes(stats.total?.data_transfer.toString()) : "inconnu"}
+                    </li>
+                    <li>
+                        <strong>Nombre de hits</strong> : {stats.total?.hits}
+                    </li>
+                </ul>
+
+                {!dataTransferChartProps ? (
+                    <LoadingText className={fr.cx("fr-ml-2w")} withSpinnerIcon as="p" />
+                ) : (
+                    <Chart {...dataTransferChartProps} name={["Volume de données transférées"]} />
+                )}
+                {!hitsChartProps ? (
+                    <LoadingText className={fr.cx("fr-ml-2w")} withSpinnerIcon as="p" />
+                ) : (
+                    <Chart {...hitsChartProps} name={["Nombre de hits"]} color={["blue-cumulus"]} />
+                )}
+            </div>
         </div>
     );
 }
