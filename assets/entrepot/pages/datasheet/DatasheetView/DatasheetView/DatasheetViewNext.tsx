d4fb2b73e614e3004cff1b91d9d5bccfd96b59ca
Orka Arnest CRUZE <orka-arnest.cruze@ign.fr>
2026-05-27 16:46:20 +0200

    feat: début nouvelle maquette fiche données #852 #1038

---
+++ assets/entrepot/pages/datasheet/DatasheetView/DatasheetView/DatasheetViewNext.tsx
@@ -0,0 +1,60 @@
+import Main from "@/components/Layout/Main";
+import { useQuery } from "@tanstack/react-query";
+import { useMemo } from "react";
+
+import { DatasheetDetailed, Metadata } from "@/@types/app";
+import api from "@/entrepot/api";
+import useCatalogueDatasheetUrl from "@/hooks/useCatalogueDatasheetUrl";
+import RQKeys from "@/modules/entrepot/RQKeys";
+import { CartesApiException } from "@/modules/jsonFetch";
+import DatasheetHeader from "./DatasheetHeader";
+
+export enum DatasheetViewActiveTabEnum {
+    Description = "description",
+    Dataset = "dataset",
+    Annexes = "annexes",
+    WMTSTMS = "wmts-tms",
+    WFS = "wfs",
+}
+
+type DatasheetViewProps = {
+    datastoreId: string;
+    datasheetName: string;
+    activeTab: string;
+};
+
+export default function DatasheetViewNext(props: DatasheetViewProps) {
+    const {
+        datastoreId,
+        datasheetName,
+        // activeTab = DatasheetViewActiveTabEnum.Description
+    } = props;
+
+    const datasheetQuery = useQuery<DatasheetDetailed, CartesApiException>({
+        queryKey: RQKeys.datastore_datasheet(datastoreId, datasheetName),
+        queryFn: ({ signal }) => api.datasheet.get(datastoreId, datasheetName, { signal }),
+        staleTime: 60000,
+        retry: false,
+        // enabled: !datasheetDeleteMutation.isPending,
+    });
+    // const datasheet = datasheetQuery.data;
+
+    const metadataQuery = useQuery<Metadata, CartesApiException>({
+        queryKey: RQKeys.datastore_datasheet_metadata(datastoreId, datasheetName),
+        queryFn: ({ signal }) => api.metadata.getByDatasheetName(datastoreId, datasheetName, { signal }),
+        // enabled: !datasheetDeleteMutation.isPending,
+        staleTime: 60000,
+        retry: false,
+    });
+    const metadata = metadataQuery.data;
+
+    const { catalogueDatasheetUrl } = useCatalogueDatasheetUrl(datastoreId, datasheetName);
+
+    const isPublished = useMemo(() => metadata?.endpoints?.length !== undefined && metadata?.endpoints?.length > 0, [metadata?.endpoints?.length]);
+
+    return (
+        <Main title={`Données ${datasheetName}`}>
+            <DatasheetHeader name={datasheetName} catalogLink={catalogueDatasheetUrl} published={isPublished} loading={datasheetQuery.isLoading} />
+        </Main>
+    );
+}
--- assets/router/GroupDatastore.tsx
+++ assets/router/GroupDatastore.tsx
@@ -13,6 +13,7 @@ const DatasheetList = lazy(() => import("../entrepot/pages/datasheet/DatasheetLi
 const DatasheetUploadForm = lazy(() => import("../entrepot/pages/datasheet/DatasheetNew/DatasheetUploadForm/DatasheetUploadForm"));
 const DatasheetUploadIntegrationPage = lazy(() => import("../entrepot/pages/datasheet/DatasheetNew/DatasheetUploadIntegration/DatasheetUploadIntegrationPage"));
 const DatasheetView = lazy(() => import("../entrepot/pages/datasheet/DatasheetView/DatasheetView/DatasheetView"));
+const DatasheetViewNext = lazy(() => import("../entrepot/pages/datasheet/DatasheetView/DatasheetView/DatasheetViewNext"));
 const StoredDataDetails = lazy(() => import("../entrepot/pages/data_details/StoredDataDetails"));
 const UploadDetails = lazy(() => import("../entrepot/pages/data_details/UploadDetails"));
 const WfsServiceForm = lazy(() => import("../entrepot/pages/service/wfs/WfsServiceForm"));
@@ -71,6 +72,16 @@ function GroupDatastore(props: IGroupDatastoreProps) {
             case "datastore_datasheet_view":
                 return {
                     render: <DatasheetView datastoreId={route.params.datastoreId} datasheetName={route.params.datasheetName} />,
+                };
+            case "datastore_datasheet_view_next":
+                return {
+                    render: (
+                        <DatasheetViewNext
+                            datastoreId={route.params.datastoreId}
+                            datasheetName={route.params.datasheetName}
+                            activeTab={route.params.activeTab}
+                        />
+                    ),
                 };
             case "datastore_stored_data_details":
                 return {
