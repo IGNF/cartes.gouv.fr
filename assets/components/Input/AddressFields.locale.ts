30b139688e341f15f1401d60f07e6adf921f9c93
Orka Arnest CRUZE <orka-arnest.cruze@ign.fr>
2026-06-23 13:39:50 +0200

    feat(producer): champ numéro+voie unifié avec autocomplétion géocodage

--- assets/components/Input/AddressFields.locale.ts
+++ assets/components/Input/AddressFields.locale.ts
@@ -1,21 +1,17 @@
 import { declareComponentKeys } from "@/i18n/i18n";
 import { Translations } from "@/i18n/types";
 
-const { i18n } = declareComponentKeys<"legend" | "field.number" | "field.street" | "field.postalCode" | "field.city">()("AddressFields");
+const { i18n } = declareComponentKeys<"legend" | "field.postalCode" | "field.city">()("AddressFields");
 export type I18n = typeof i18n;
 
 export const AddressFieldsFrTranslations: Translations<"fr">["AddressFields"] = {
     legend: "Adresse postale",
-    "field.number": "Numéro",
-    "field.street": "Voie",
     "field.postalCode": "Code Postal",
-    "field.city": "Ville",
+    "field.city": "Ville ou commune",
 };
 
 export const AddressFieldsEnTranslations: Translations<"en">["AddressFields"] = {
     legend: "Postal address",
-    "field.number": "Number",
-    "field.street": "Street",
     "field.postalCode": "Postal code",
-    "field.city": "City",
+    "field.city": "City or municipality",
 };
