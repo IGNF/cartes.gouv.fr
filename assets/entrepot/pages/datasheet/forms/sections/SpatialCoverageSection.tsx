a2d17a12f2e9b0c7789de175af199844585189ad
Orka Arnest CRUZE <orka-arnest.cruze@ign.fr>
2026-07-28 17:37:32 +0200

    feat: champ territoires autocomplétion, ajout code insee des collectivités #1102

--- assets/entrepot/pages/datasheet/forms/sections/SpatialCoverageSection.tsx
+++ assets/entrepot/pages/datasheet/forms/sections/SpatialCoverageSection.tsx
@@ -3,9 +3,10 @@ import { Controller, useFormContext } from "react-hook-form";
 
 import AsyncAutocompleteSelect from "@/components/Input/AsyncAutocompleteSelect";
 import api from "@/entrepot/api";
+import type { AdminUnitTerritory } from "@/entrepot/api/geocoding";
 import { useTranslation } from "@/i18n/i18n";
 import RQKeys from "@/modules/entrepot/RQKeys";
-import { type Territory, MetadataFormValues } from "../metadataSchema";
+import type { MetadataFormValues } from "../metadataSchema";
 
 export default function SpatialCoverageSection() {
     const { t } = useTranslation("DatasheetSections");
@@ -20,12 +21,19 @@ export default function SpatialCoverageSection() {
                 control={control}
                 name="territories"
                 render={({ field, fieldState: { error } }) => (
-                    <AsyncAutocompleteSelect<Territory, true>
+                    <AsyncAutocompleteSelect<AdminUnitTerritory, true>
                         label={t("field.territories")}
                         hintText={t("field.territories.hint")}
                         queryKey={(s) => RQKeys.search_territories(s)}
                         queryFn={(s, signal) => api.geocoding.searchAdminUnits(s, signal)}
                         getOptionLabel={(option) => option.title}
+                        renderOption={(props, option) => (
+                            <li {...props} key={option.id}>
+                                {option.title}
+                                {/* Code INSEE affiché dans la liste uniquement (pas dans le tag) pour distinguer les homonymes */}
+                                {option.insee && <span className={fr.cx("fr-ml-1v", "fr-mb-0")}>({option.insee})</span>}
+                            </li>
+                        )}
                         getOptionKey={(option) => option.id}
                         isOptionEqualToValue={(option, value) => option.id === value.id}
                         multiple={true}
