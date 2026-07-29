60184ac40209af6eb22f249dc82f80c5cfd7de63
Orka Arnest CRUZE <orka-arnest.cruze@ign.fr>
2026-07-29 12:13:49 +0200

    fix(upload): réutilisation du tag theme existant à la place du tag themes

--- src/Constants/EntrepotApi/CommonTags.php
+++ src/Constants/EntrepotApi/CommonTags.php
@@ -10,5 +10,4 @@ final class CommonTags
     public const PRODUCER_SHORT = 'producer_short';
     public const PRODUCTION_YEAR = 'production_year';
     public const PRODUCTION_DATE = 'date';
-    public const THEMES = 'themes';
 }
